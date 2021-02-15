2eb52c298758e30c2d9f02a3ccefbd715c780d24
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-02-15 10:54:57 +0530

    Time: 0 ms (100.00%), Space: 39.7 MB (20.68%) - LeetHub

--- peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
+++ peak-index-in-a-mountain-array/peak-index-in-a-mountain-array.java
@@ -1,5 +1,6 @@
 class Solution {
     public int peakIndexInMountainArray(int[] arr) {
+        /*
         int i=0;
         int ans = 0;
         while(i<arr.length-1 && arr[i]<arr[i+1])
@@ -7,5 +8,25 @@ class Solution {
                 ans=i++ +1;
         
         return ans;
+        */
+        
+        int lo=0;
+        int hi=arr.length-1;
+        int ans=0;
+        while(lo<=hi)
+        {
+            int mid = lo + (hi-lo)/2;
+            if(mid>0 && arr[mid]<arr[mid-1])
+            {
+                hi=mid-1;
+            }
+            else
+            {
+                ans=Math.max(ans,mid);
+                lo=mid+1;
+            }
+        }
+        
+        return hi;
     }
 }
