381e79ac4046a9acd58fea82c5ebba4e2341fe18
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-02-16 07:30:05 +0530

    Time: 17 ms (14.43%), Space: 35.7 MB (75.17%) - LeetHub

--- kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
+++ kth-smallest-number-in-multiplication-table/kth-smallest-number-in-multiplication-table.java
@@ -7,24 +7,17 @@ class Solution {
         while(lo<=hi)
         {
             int mid=lo + (hi-lo)/2;
-            int less=0;
-            int more=0;
+            int res=0;
             for(int i=1;i<=n;i++)
-            {
-                int find = mid-1;
-               less = less + Math.min(find/i,m);
-                more=more+Math.min(mid/i,m);
-            }
-            
-          //  System.out.println(mid+" "+less+" "+more);
-            if(k>less && k<=more)
-                return mid;
-            else if(less<k)
+               res +=Math.min(mid/i,m);            
+
+           
+             if(res<k)
                 lo=mid+1;
             else
                 hi=mid-1;
         }
         
-        return 0;
+        return lo;
     }
 }
