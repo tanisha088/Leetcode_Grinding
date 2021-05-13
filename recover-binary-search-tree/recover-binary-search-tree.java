7fc51a156da8d1f4a5de688d9fcba6eb259de97c
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-05-13 16:57:15 +0530

    Time: 2 ms (84.03%), Space: 39.1 MB (71.26%) - LeetHub

--- recover-binary-search-tree/recover-binary-search-tree.java
+++ recover-binary-search-tree/recover-binary-search-tree.java
@@ -17,40 +17,64 @@
 
 
 class Solution {
-    TreeNode prev=null;
-    TreeNode first=null;
-    TreeNode second=null;
+    
+    
     public void recoverTree(TreeNode root) {
 
-        rt(root);
+        TreeNode prev =null;
+        TreeNode first=null;
+        TreeNode second =null;
+        while(root!=null)
+        {
+            if(root.left==null)
+            {
+                if(prev!=null)
+                    if(root.val<prev.val)
+                    {
+                        if(first==null)
+                            first=prev;
+                    if(first!=null)
+                     second=root;
+                    }
+                prev=root;
+                
+                root=root.right;
+            } 
+                else
+                {
+                    TreeNode m = root.left;
+                    while(m.right!=null && m.right!=root)
+                    {
+                        m=m.right;
+                    }
+                    if(m.right==null)
+                    {
+                        m.right=root;
+                        root=root.left;
+                    }
+                    else
+                    {
+                        
+                     if(prev!=null)
+                    if(root.val<prev.val)
+                    {
+                        if(first==null)
+                            first=prev;
+                       if(first!=null) 
+                     second=root;
+                    }
+                        prev=root;
+                        m.right=null;
+                        root=root.right;
+                        
+                    }
+                }
+            
+            }
         
-        int t = first.val;
+         int t = first.val;
         first.val = second.val;
         second.val = t;
-    }
-    
-    public void rt(TreeNode root)
-    {
-        if(root==null)
-            return;
-        
-        rt(root.left);
-        
-        if(prev!=null)
-       if(root.val<prev.val)
-        {
-            if(first==null)
-            {
-                first = prev;
-            }
-             if(first!=null)
-                second = root;
-        }
-        
-        prev= root;
-        
-        rt(root.right);
-        
     }
         
 }
