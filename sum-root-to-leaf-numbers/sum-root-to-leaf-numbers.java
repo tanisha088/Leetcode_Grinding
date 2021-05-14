e833d8520fed6a787c330d921df738b37bcca502
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-05-14 11:20:06 +0530

    Time: 0 ms (100.00%), Space: 36.3 MB (78.65%) - LeetHub

--- sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
+++ sum-root-to-leaf-numbers/sum-root-to-leaf-numbers.java
@@ -15,17 +15,37 @@
  */
 class Solution {
     public int sumNumbers(TreeNode root) {
+      
         
+        
+        // 1ST APPROACH ---> TAKE SAARA NODE TO LEAF PATH IN A LIST AND THEN UNKA SUM NIKAL LO
+        /*
         List<String> list= new ArrayList<String>();
-        
         fn(root,"",list);
-        
         int sum=0;
-        
         for(int i=0;i<list.size();i++)
             sum=sum+Integer.parseInt(list.get(i));
-        
         return sum;
+        */
+        
+        return summing(root,0);
+    }
+    
+    public int summing(TreeNode root,int sum)
+    {
+        if(root==null)
+            return 0;
+        
+        if(root.left==null && root.right==null)
+        {
+            sum = sum*10+root.val;
+            return sum;
+        }
+        
+        int l = summing(root.left,sum*10+root.val);
+        int r = summing(root.right,sum*10+root.val);
+        
+        return l+r;
     }
     
     public void fn(TreeNode root,String str,List<String> list)
