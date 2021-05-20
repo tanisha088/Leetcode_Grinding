0f7627652184b99ebd52a0a461f34b3d103aa15e
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-05-20 10:29:03 +0530

    Time: 3 ms (45.06%), Space: 39.8 MB (27.77%) - LeetHub

--- max-area-of-island/max-area-of-island.java
+++ max-area-of-island/max-area-of-island.java
@@ -1,18 +1,17 @@
 class Solution {
     
-    int res=0;
     public int maxAreaOfIsland(int[][] grid) {
       
         int con=0;
-        boolean[][] vertices= new boolean[grid.length][grid[0].length];
+      //  boolean[][] vertices= new boolean[grid.length][grid[0].length];
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
-                if(vertices[i][j]==false && grid[i][j]==1)
+                if( grid[i][j]==1)
                 {
-                    res=0;
-               dfs(i,j,grid.length,grid[0].length,grid,vertices);
+                   
+              int res = dfs(i,j,grid.length,grid[0].length,grid);
                   con=Math.max(con,res);
                 }
             }
@@ -21,25 +20,25 @@ class Solution {
         return con;
     }
 
-    public void dfs(int i,int j,int n,int m, int[][] grid,boolean[][] vertices)
+    public int dfs(int i,int j,int n,int m, int[][] grid)
     {
-        vertices[i][j]=true;
-        res++;
+        grid[i][j]=0;
         
         int[] x= {-1,0,1,0};
         int[] y= {0,-1,0,1};
 
+        int ans = 1;
         for(int k=0;k<4;k++)
         {
             int nx = x[k]+i;
             int ny = y[k]+j;
           
-            if(nx>=0 && nx<n && ny>=0 && ny<m && vertices[nx][ny]==false && grid[nx][ny]==1)
+            if(nx>=0 && nx<n && ny>=0 && ny<m  && grid[nx][ny]==1)
             {
-                vertices[nx][ny]=true;
-                dfs(nx,ny,n,m,grid,vertices);
+               
+               ans =ans+ dfs(nx,ny,n,m,grid);
             }
         }
-
+     return ans;
     }
 }
