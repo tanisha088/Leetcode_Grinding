537b7b29859cd031fc969cbdab3593f72b1da2bb
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-05-22 06:27:03 +0530

    Time: 75 ms (49.80%), Space: 42 MB (39.01%) - LeetHub

---
+++ word-ladder/word-ladder.java
@@ -0,0 +1,53 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        
+        LinkedList<String> list= new LinkedList<String>();
+     HashSet<String> wordlists= new HashSet<>(wordList);
+        
+        list.add(beginWord);
+        HashSet<String> set= new HashSet<String>();
+        int ans=0;
+        while(!list.isEmpty())
+        {
+            int s = list.size();
+              ans++;
+            while(s!=0)
+            {
+            String a = list.remove();
+        //  System.out.println(a);
+            if(a.equals(endWord))
+                return ans;
+      
+            char[] charray= a.toCharArray();
+            for(int i=0;i<a.length();i++)
+            {
+              char orig=  charray[i];
+                for(int j=0;j<26;j++)
+                {
+                   
+                    charray[i]=(char)(j+'a');
+                    String s1 = String.valueOf(charray);
+                    // String s1 = new String(charray);
+                    
+                 //   System.out.println(s1);
+                    if(!wordlists.contains(s1))
+                        continue;
+                        
+                    if(!set.contains(s1))
+                    list.add(s1);
+                    set.add(s1);
+                
+                }
+                
+                charray[i]=orig;
+            }
+                s--;
+            }
+    //        System.out.println("(((((((())))))))");
+                
+            
+        }
+        
+        return 0;
+    }
+}
