de4d25ff915df441d64be27caf9a3c42509dc1f6
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-11 08:52:25 +0530

    Time: 0 ms (100.00%), Space: 39.4 MB (14.64%) - LeetHub

--- reverse-linked-list/reverse-linked-list.java
+++ reverse-linked-list/reverse-linked-list.java
@@ -11,6 +11,10 @@
 class Solution {
     public ListNode reverseList(ListNode head) {
         
+        
+     //   1ST SOLUTION ---> ITERATIVE
+        
+        /*
         if(head==null || head.next==null)
             return head;
         
@@ -18,8 +22,10 @@ class Solution {
         head.next.next=head;
         head.next=null;
         return newHead;
+        */
         
-        
+            
+    //        2ND SOLUTION -> RECURSIVEE
         /*
         // RECURSIVE
         //return reverseLists(null,head);
@@ -54,6 +60,27 @@ class Solution {
         
         return curr;
         */
+            
+            
+       //     3RD SOLUTION-- -> 
+
+          if(head==null || head.next==null)
+              return head;
+             ListNode dummy = new ListNode(0);
+        dummy.next=head;
+        head=dummy;
+    
+              ListNode preMiddle=dummy;
+            ListNode preCurrent=dummy.next;
+            while(preCurrent.next!=null){
+            ListNode current = preCurrent.next;
+                preCurrent.next=current.next; // ye phle kardia isse ek extra pointer lene se bach gaye
+                 current.next=preMiddle.next;
+                preMiddle.next=current;
+               
+            }
+        
+        return dummy.next;
     }
     
     public ListNode reverseLists(ListNode prev,ListNode curr)
