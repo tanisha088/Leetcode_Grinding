e32a8f5aafa5d7de36565c70da273be3f7998421
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-26 00:06:57 +0530

    Time: 1 ms (100.00%), Space: 35.8 MB (75.59%) - LeetHub

--- ugly-number/ugly-number.java
+++ ugly-number/ugly-number.java
@@ -1,8 +1,48 @@
 class Solution {
     public boolean isUgly(int n) {
+        
+        // ek number bina uske prime factors ke ban hi nhi skta toh if ek number 2,3,5, se divisble hai to usse divide kar do and if agar koi point aaye jaha wo number kisi bhi 2,3,5 se divisible na ho then wo number ugly nhi hai --- and since agar 2,3,5 ke alawa ek bhi prime factor hoga kisi bhi number ka then wo pakka encounter hoga hi hoga.
+        
+        
+        /*
         if(n<=0)
             return false;
-        n = Math.abs(n);
+       
+        while(n!=1)
+        {
+            if(n%2==0)
+                n/=2;
+            else if(n%3==0)
+                n/=3;
+            else if(n%5==0)
+                n/=5;
+            else
+                return false;
+        }
+        
+        return true;
+        
+        */
+        
+        
+  while(n>1 && n%2==0)
+  {
+      n=n/2;
+  }
+
+while(n>1 && n%3==0)
+{
+    n=n/3;
+}
+while(n>1 && n%5==0)
+    n=n/5;
+
+///System.out.println(n);
+if(n>1 || n<=0)
+    return false;
+return true;
+        
+          /*
         
         if(n==1 || n==2 || n==3 || n==5)
             return true;
@@ -15,6 +55,10 @@ class Solution {
             return isUgly(n/5);
         else
             return false;
+            */
     }
     
 }
+
+
+
