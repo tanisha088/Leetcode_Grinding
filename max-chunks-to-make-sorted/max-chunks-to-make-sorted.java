323683e7c5e0afb7835a970fa089ee67e5d78c9b
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-27 02:08:46 +0530

    Time: 0 ms (100.00%), Space: 36.5 MB (23.66%) - LeetHub

--- max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
+++ max-chunks-to-make-sorted/max-chunks-to-make-sorted.java
@@ -1,6 +1,6 @@
 class Solution {
     public int maxChunksToSorted(int[] arr) {
-        
+        /*
         int max=0;
         int chunk=1;
         
@@ -14,3 +14,44 @@ class Solution {
         return chunk;
     }
 }
+*/
+
+int st=0;
+int end=0;
+    int res=0;
+while(end<arr.length)
+{
+    int min=arr[st];
+    int max=arr[st];
+    
+    while(end<arr.length-1 && (max!=end || st!=min))
+    {
+        end++;
+        max=Math.max(max,arr[end]);
+        min=Math.min(min,arr[end]);
+    }
+   st=end+1;
+       end+=1;
+       res+=1;
+}      
+     
+return res;
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
