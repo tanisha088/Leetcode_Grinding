3c6a2baa0356de06395df1b6f40744b3a366bfab
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-27 00:14:55 +0530

    Time: 1 ms (99.90%), Space: 42.5 MB (63.43%) - LeetHub

--- majority-element/majority-element.java
+++ majority-element/majority-element.java
@@ -1,6 +1,6 @@
 class Solution {
     public int majorityElement(int[] nums) {
-       
+       /*
         int fp=0;
       
         int fc=0;
@@ -19,6 +19,25 @@ class Solution {
         }
         
         return fp;
+        */
+        
+        int v=nums[0];
+        int c=1;
+        
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]==v)
+                c++;
+            else
+                c--;
+            if(c==0)
+            {
+                v=nums[i];
+                c=1;
+            }
+        }
+        
+        return v;
     }
 }
 
