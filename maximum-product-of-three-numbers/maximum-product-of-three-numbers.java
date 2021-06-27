2875a1dce45a9370e4adcf1f7c57fc7987c35cf6
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-27 12:09:16 +0530

    Time: 9 ms (63.69%), Space: 40.5 MB (49.79%) - LeetHub

--- maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
+++ maximum-product-of-three-numbers/maximum-product-of-three-numbers.java
@@ -1,9 +1,30 @@
 class Solution {
     public int maximumProduct(int[] nums) {
-        
+        /*
         PriorityQueue<Integer> pq= new PriorityQueue<Integer>();
                 PriorityQueue<Integer> pq1= new PriorityQueue<Integer>((a,b)->(b-a));
 
+        
+              for(int i=0;i<nums.length;i++)
+              {
+                  if(pq.size()<3)
+                      pq.add(nums[i]);
+                  else if(pq.peek()<nums[i])
+                  {
+                      pq.poll();
+                      pq.add(nums[i]);
+                  }
+                  
+                  if(pq1.size()<2)
+                      pq1.add(nums[i]);
+                  else if(pq1.peek()<nums[i])
+                  {
+                      pq1.add(nums[i]);
+                  }
+              }
+        
+        int max=0
+        /*
         int mx1=Integer.MIN_VALUE;
         int mx2=Integer.MIN_VALUE;
             int mx3=Integer.MIN_VALUE;
@@ -37,7 +58,72 @@ class Solution {
         }
         
         return Math.max(mx1*mx2*mx3 ,mx1*mi1*mi2);
+        */
         
-        
+        /*
+         PriorityQueue<Integer> poheap = new PriorityQueue<>();
+        PriorityQueue<Integer> neheap = new PriorityQueue<>(Collections.reverseOrder());
+        for (int num : nums) {
+            poheap.offer(num);
+            neheap.offer(num);
+            if (poheap.size() > 3) {
+                poheap.poll();
+            }
+            if (neheap.size() > 2) {
+                neheap.poll();
+            }
+        }
+        int c1 = 1;
+        int max = 0;
+        while (!poheap.isEmpty()) {
+            max = poheap.poll();
+            c1 *= max;
+        }
+        System.out.println(c1+" "+max);
+        while (!neheap.isEmpty()) {
+            max *= neheap.poll();
+        }
+        return Math.max(c1, max);
     }
 }
+
+
+*/
+
+
+
+Arrays.sort(nums);
+
+
+ 
+   return Math.max(nums[nums.length-1]*nums[nums.length-2]*nums[nums.length-3],nums[0]*nums[1]*nums[nums.length-1]);
+
+    }
+}
+
+
+
+
+
+
+
+//-4 -2 2 4
+
+/*
+a b -------c------d e
+
+3 elements from end=> if +ve --true
+    if(rend<0)
+        choose the last 3
+    else if(r2end<0)
+2 elements from lend and 1 from rend 
+    
+
+-5 -4 -3 -2 -1
+
+*/
+
+
+
+
+
