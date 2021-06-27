66208296d1116dae04a938c07abbf384745f2f8e
tanisha088 <31798751+tanisha088@users.noreply.github.com>
2021-06-27 13:07:07 +0530

    Time: 0 ms (100.00%), Space: 36.6 MB (71.64%) - LeetHub

--- largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
+++ largest-number-at-least-twice-of-others/largest-number-at-least-twice-of-others.java
@@ -1,5 +1,6 @@
 class Solution {
     public int dominantIndex(int[] nums) {
+        /*
         int max1=Integer.MIN_VALUE;
         int max2=Integer.MIN_VALUE;
         int in1=-1;
@@ -22,5 +23,32 @@ class Solution {
         }
         System.out.println(max1+" "+max2);
         return (max1>=(2*max2))?in1:-1;
+        
+        */
+        
+        if(nums.length==1)
+            return 0;
+        
+        int ind=1;
+        int max=Math.max(nums[0],nums[1]);
+        if(nums[0]>nums[1])
+            ind=0;
+        int min=Math.min(nums[0],nums[1]);
+        
+        for(int i=2;i<nums.length;i++)
+        {
+            if(nums[i]>max)
+            {
+                min=max;
+                max=nums[i];
+                ind=i;
+            }
+            else if(nums[i]>min)
+            { 
+                min = nums[i];
+            }
+        }
+        
+        return (max>=2*min)?ind:-1;
     }
 }
