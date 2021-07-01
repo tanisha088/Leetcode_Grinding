5fcc81ac5565943252ff30a7f4fb2375cdc8b780
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-01 18:43:49 +0530

    Time: 3 ms (47.47%), Space: 86.1 MB (59.49%) - LeetHub

--- first-missing-positive/first-missing-positive.java
+++ first-missing-positive/first-missing-positive.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int firstMissingPositive(int[] nums) {
+    public int firstMissingPositive(int[] arr) {
      
+        /*
         for(int i=0;i<nums.length;i++)
             nums[i] = (nums[i]<=0)?nums.length+2:nums[i];
         
@@ -18,6 +19,71 @@ class Solution {
             if(nums[i]>0)
                 return i+1;
         return nums.length+1;
-     
+     */
+        
+        for(int i=0;i<arr.length;i++)
+            if(arr[i]<=0 || arr[i]>arr.length)
+                arr[i]=arr.length+2;
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(Math.abs(arr[i])<=arr.length && arr[Math.abs(arr[i])-1]>0)
+            {
+                arr[Math.abs(arr[i])-1]=0-arr[Math.abs(arr[i])-1];
+            }
+        }
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            if(arr[i]>0)
+                return i+1;
+        }
+        return arr.length+1;
+      
     }
 }
+
+/*
+
+-ve + >arr.length
+
+arr.length+2 
+
+
+
+
+
+
+1->arr.length+1
+
+val -> !max && <=arr.length ->arr[val-1] = min
+
+i+1 == max 
+
+-ve -> max
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
