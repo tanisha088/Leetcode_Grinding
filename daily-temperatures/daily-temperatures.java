ff8e933a1c66605b22291acf9af693a5455ed9dc
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-02 20:08:18 +0530

    Time: 37 ms (36.89%), Space: 48.5 MB (44.61%) - LeetHub

--- daily-temperatures/daily-temperatures.java
+++ daily-temperatures/daily-temperatures.java
@@ -1,5 +1,6 @@
 class Solution {
-    public int[] dailyTemperatures(int[] nums2) {
+    public int[] dailyTemperatures(int[] nums) {
+        /*
          int[] result = new int[nums2.length];
         Stack<Integer> st= new Stack<Integer>();
         for(int i=nums2.length-1;i>=0;i--)
@@ -34,6 +35,30 @@ class Solution {
         }
         
         return result;
+        */
+          
+         Stack<Integer> st = new Stack<Integer>();
+       
+       int[] res= new int[nums.length];
+       for(int i=0;i<nums.length;i++)
+       res[i]=0;
+       
+       for(int i=nums.length-1;i>=0;i--)
+       {
+         //  if(!st.isEmpty && st.peek()>arr[i])
+          //  res[i]=st.peek();
+           
+           while(!st.isEmpty() && nums[st.peek()]<=nums[i])
+           {
+           st.pop();
+           }           
+           
+           
+           if(!st.isEmpty() && i<nums.length)
+             res[i]=st.peek()-i;
+           st.push(i);
+       }
         
+        return res;
     }
 }
