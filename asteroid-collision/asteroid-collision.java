708b59c0a9c48933e7d7b638826f6ee64264d785
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-03 01:02:21 +0530

    Time: 5 ms (64.59%), Space: 39.2 MB (96.14%) - LeetHub

---
+++ asteroid-collision/asteroid-collision.java
@@ -0,0 +1,51 @@
+class Solution {
+    public int[] asteroidCollision(int[] asteroids) {
+     /*
+        Stack<Integer> st= new Stack<Integer>();
+        for(int i=0;i<asteroids.length;i++)
+        {
+            if(asteroids[i]>0)
+                st.push(asteroids[i]);
+            else
+            {
+               while(!st.isEmpty() && Math.abs(asteroids[i])>st.peek())
+               {
+                   st.pop();
+               }
+                if(st.peek()==Math.abs(asteroids[i]))
+                    st.pop();
+            }
+        }
+        */
+        
+         Stack<Integer> st= new Stack<Integer>();
+        for(int i=0;i<asteroids.length;i++)
+        {
+           if(asteroids[i]<0 && !st.isEmpty() && st.peek()>0)
+           {
+               int m = Math.abs(asteroids[i]);
+               while(!st.isEmpty() && st.peek()>0 && st.peek()<m)
+                   st.pop();
+               
+               if(!st.isEmpty() && st.peek()==m)
+                   st.pop();
+               else if(st.isEmpty() || (st.peek()<m))
+                   st.push(asteroids[i]);
+           }
+            else
+                st.push(asteroids[i]);
+        }
+        
+        int[] arr = new int[st.size()];
+        int i=st.size()-1;
+        while(!st.isEmpty())
+        {
+           arr[i]=st.pop();
+            i--;
+        }
+        
+        return arr;
+    }
+}
+
+//10 5 -5 -10
