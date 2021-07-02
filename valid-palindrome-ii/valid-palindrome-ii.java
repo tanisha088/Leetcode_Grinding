a7d10d0063de10091ef920072d3324a5e78d98b5
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-02 07:29:50 +0530

    Time: 12 ms (16.82%), Space: 39.3 MB (78.03%) - LeetHub

--- valid-palindrome-ii/valid-palindrome-ii.java
+++ valid-palindrome-ii/valid-palindrome-ii.java
@@ -1,6 +1,46 @@
 class Solution {
     public boolean validPalindrome(String s) {
+        return  vP1(s,1);
+    }
+    
+    public boolean vP1(String s,int cap)
+    {
+           char[] ar = s.toCharArray();
         
+        if(cap<0)
+            return false;
+        
+       if( ispal(ar,-1))
+       {
+     //      System.out.println(s+" " +ispal(ar,-1));   
+           return true;
+       }
+        
+        int i=0;
+        int j=s.length()-1;
+        
+        while(i<j)
+        {
+            if(ar[i]!=ar[j])
+            {
+                  
+                if(vP1(s.substring(i+1,j+1),cap-1) || vP1(s.substring(i,j),cap-1))                     return true;
+                
+                return false;
+            }
+            else 
+            {
+                i++;
+                j--;
+            }
+        }
+        
+                return false;
+
+    }   
+        
+        
+        /*
         char[] ar = s.toCharArray();
         
        if( ispal(ar,-1) )
@@ -28,7 +68,7 @@ class Solution {
         
         return false;
     }
-    
+    */
     public boolean ispal(char[] arr ,int ig)
     {
         int i=0;
@@ -52,5 +92,6 @@ class Solution {
         
         return true;
     }
+    
 }
 
