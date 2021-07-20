9b5af838d72cce054719bf34e962dafc917916d6
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-20 18:57:58 +0530

    Time: 0 ms (100.00%), Space: 36.6 MB (84.65%) - LeetHub

--- binary-tree-coloring-game/binary-tree-coloring-game.java
+++ binary-tree-coloring-game/binary-tree-coloring-game.java
@@ -16,6 +16,7 @@
 class Solution {
     public boolean btreeGameWinningMove(TreeNode root, int n, int x) {
         
+        /*
         TreeNode nodex =  find_node(root,x);
         
         int leftsubtree =  count(nodex.left,0);
@@ -58,5 +59,72 @@ class Solution {
         int b = count(root.right,c);
         
         return a+b+1;
+    }
+    */
+        
+        
+   //     **************************** WRONG SOLUTION **********************************
+//         if(root.val==x)
+//             return false;
+//         int[]  arr = new int[1];
+//         arr[0]=-1;
+//         int c = count_nodes(root,arr,x);
+        
+//         int onesubtree = c-arr[0];
+        
+//         if(onesubtree == arr[0])
+//             return false;
+        
+//         return true;
+//     }
+    
+//     public int count_nodes(TreeNode root,int[] arr,int x)
+//     {
+//         if(root==null)
+//             return 0;
+        
+//         int c1 =  count_nodes(root.left,arr,x);
+//         int c2 = count_nodes(root.right,arr,x);
+        
+//         int ans = c1+  c2 + 1;
+        
+//         if(x==root.val)
+//         {
+//             arr[0]=ans;
+//         }
+        
+//         return ans;
+        
+//     }
+        
+     // **************************** WRONG SOLUTION **********************************
+        
+     
+        int[] arr =  new int[]{0,0,0};
+        
+      int m =   counts(root,arr,x);
+        
+        int par = m - arr[0];
+        
+        return Math.max(par,Math.max(arr[1],arr[2])) > m/2 ? true:false;
+        
+    }
+    
+    public int counts(TreeNode root,int[] arr,int x)
+    {
+        if(root==null)
+            return 0;
+        
+        int l = counts(root.left,arr,x);
+        int r=  counts(root.right,arr,x);
+        
+        if(root.val==x)
+        {
+            arr[0]=l+r+1;
+            arr[1]=l;
+            arr[2]=r;
+        }
+        
+        return l+r+1;
     }
 }
