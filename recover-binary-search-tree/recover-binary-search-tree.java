d41820ed5207f54f7c528e10d1dd791c7f01407e
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-21 08:15:55 +0530

    Time: 2 ms (86.23%), Space: 39.5 MB (45.88%) - LeetHub

--- recover-binary-search-tree/recover-binary-search-tree.java
+++ recover-binary-search-tree/recover-binary-search-tree.java
@@ -21,6 +21,77 @@ class Solution {
     
     public void recoverTree(TreeNode root) {
 
+        TreeNode prev= null;
+        TreeNode first =null;
+        TreeNode second = null;
+        
+        while(root!=null)
+        {
+            if(root.left!=null)
+            {
+                TreeNode m = root.left;
+                while(m.right!=null && m.right!=root)
+                    m=m.right;
+                
+                if(m.right==null)
+                {
+                    m.right = root;
+                    root=root.left;
+                }
+                else
+                {
+                     
+                       if(prev.val>root.val)
+                {
+                 ///                       System.out.println(prev.val+" "+root.val+" ***"+" ");
+
+                    if(first==null)
+                    {
+                        first = prev;
+                        second = root;
+                    }
+                    else
+                    {
+                        second = root;
+                    }
+                           
+                                    
+                }
+                    prev= root;
+                   root  = root.right;
+                    m.right=null;
+                }
+            }
+            else
+            {
+                
+                if(prev!=null && prev.val>root.val)
+                {
+                    if(first==null)
+                    {
+                        first = prev;
+                        second = root;
+                    }
+                    else
+                    {
+                        second = root;
+                    }
+                }
+                   
+              
+                prev=root;
+               root=root.right;
+            }
+        }
+        
+      //  System.out.println(first.val+" "+second.val);
+        int val = first.val;
+        first.val =  second.val;
+        second.val = val;
+        
+        
+        
+        /*
         TreeNode prev =null;
         TreeNode first=null;
         TreeNode second =null;
@@ -75,6 +146,8 @@ class Solution {
          int t = first.val;
         first.val = second.val;
         second.val = t;
+        */
     }
+    
         
 }
