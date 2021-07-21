4c99b334d9539d7234e9a40f18c8fcad9599bad7
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-21 08:55:07 +0530

    Time: 3 ms (95.24%), Space: 39.1 MB (59.95%) - LeetHub

--- subtree-of-another-tree/subtree-of-another-tree.java
+++ subtree-of-another-tree/subtree-of-another-tree.java
@@ -16,12 +16,39 @@
 class Solution {
     public boolean isSubtree(TreeNode root, TreeNode subRoot) {
         
+        if(root==null)
+            return false;
+        
+        
+        if(root.val == subRoot.val && isequal(root,subRoot)==true)
+          return true;
+        
+        return isSubtree(root.left,subRoot) || isSubtree(root.right,subRoot);
+           
+    }
+    
+    public boolean isequal(TreeNode r1,TreeNode r2)
+    {
+        if(r1==null && r2==null)
+            return true;
+        
+        if(r1==null || r2==null)
+            return false;
+        
+        if(r1.val!=r2.val)
+            return false;
+        
+        return isequal(r1.left,r2.left) && isequal(r1.right,r2.right);
+    }
+        
+        /*
        if(root==null)
             return false;
 
         if(root.val==subRoot.val)
         {
-            if(isequal(root,subRoot))
+            if(isequal(root,subRoot)) // NO NEED TO EVEN CHECK FOR THIS SINCE AGAR DIRECT BHI IS EQUAL CALL KIYA TO WO FIRST CALL MEI HI SAME NA HONE PE RETURN FALSE KAR DEGA
+                // ONE MORE THING KI AGAR WO ISEQUAL KA RESULT FALSE DE TO RETURN FALSE NHI KARNA .. THIS IS BECAUSE AISA HO SKTA HAI KI HUME DONO KA ROOT EQUAL MIL JAAYE BUT ANDAR KA SUBTREE DIFF HO BUT USKE KOI CHILD MEI SAME SUBTREE PRESENT HO .. SO AGAR TRUE MILTA HAI TO TRUE RETURN .. ELSE NEECHE MEI CHECK KARO
                 return true;
         }
         
@@ -43,4 +70,6 @@ class Solution {
         return false;
             
     }
+    
+    */
 }
