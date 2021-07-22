9cdabe6287549160d2c7000a0ada1484729ed7de
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-22 10:10:23 +0530

    Time: 0 ms (100.00%), Space: 39.6 MB (43.65%) - LeetHub

--- delete-node-in-a-bst/delete-node-in-a-bst.java
+++ delete-node-in-a-bst/delete-node-in-a-bst.java
@@ -14,44 +14,94 @@
  * }
  */
 class Solution {
-    TreeNode m = null;
-    public TreeNode deleteNode(TreeNode root, int key) {
-        
-        if(root==null)
-            return root;
-        
-        if(root.val>key)
-        {
-            root.left = deleteNode(root.left,key);
-        }
-        else if(root.val<key)
-        {
-            root.right = deleteNode(root.right,key);
-        }
-        else
-        {
-            root = delete(root);
-        }
-        
-        return root;
-    }
     
-    public TreeNode delete(TreeNode root)
-    {
-         if(root.left==null)
-             return root.right;
-        if(root.right==null)
-            return root.left;
+        public TreeNode deleteNode(TreeNode root, int key) {
+          
+            if(root==null)
+                return root;
+            
+            if(root.val>key)
+            {
+                root.left =  deleteNode(root.left,key);
+            }
+            else if(root.val<key)
+            {
+                root.right = deleteNode(root.right,key);
+            }
+            else
+            {
+                return del(root);
+            }
+            
+            return root;
+        }
+    
+    
+        public TreeNode del(TreeNode root)
+        {
+            if(root.left==null && root.right==null)
+                return null;
+            
+            else if(root.left==null)
+                return root.right;
+            else if(root.right==null)
+                return root.left;
+            else
+            {
+                TreeNode m = root.left;
+                while(m.right!=null)
+                    m=m.right;
+                root.val = m.val;
+               root.left =  deleteNode(root.left,m.val);
+                
+                return root;
+            }
+        }
+    
+    
+    
+    
+    
+    
+//     TreeNode m = null;
+//     public TreeNode deleteNode(TreeNode root, int key) {
         
-        TreeNode k = root.left;
-        while(k.right!=null)
-            k=k.right;
+//         if(root==null)
+//             return root;
         
-        int nv = k.val;
-        root.val = nv;
+//         if(root.val>key)
+//         {
+//             root.left = deleteNode(root.left,key);
+//         }
+//         else if(root.val<key)
+//         {
+//             root.right = deleteNode(root.right,key);
+//         }
+//         else
+//         {
+//             root = delete(root);
+//         }
         
-        root.left  = deleteNode(root.left,nv);
+//         return root;
+//     }
+    
+//     public TreeNode delete(TreeNode root)
+//     {
+//          if(root.left==null)
+//              return root.right;
+//         if(root.right==null)
+//             return root.left;
         
-        return root;
-    }
+//         TreeNode k = root.left;
+//         while(k.right!=null)
+//             k=k.right;
+        
+//         int nv = k.val;
+//         root.val = nv;
+        
+//         // V IMP TO SEE THAT 
+//         root.left  = deleteNode(root.left,nv);
+        
+//         return root;
+//     }
 }
