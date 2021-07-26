d6ee1b104413de4b60f4d3768196380f08204049
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-26 14:53:15 +0530

    Time: 13 ms (97.52%), Space: 38.9 MB (86.60%) - LeetHub

---
+++ all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.java
@@ -0,0 +1,83 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    
+           HashMap<Integer,ArrayList<Integer>> h = new HashMap();
+     
+   List<Integer> res=  new ArrayList<Integer>();
+    public List<Integer> distanceK(TreeNode root, TreeNode target, int k) {
+      
+        if(k==0)
+        {
+            res.add(target.val);
+            return res;
+        }
+        
+                 fn(root,target,k);
+        
+        return res;
+    }
+   int fn(TreeNode node,TreeNode targ,int k)
+   {
+       if(node==null)
+           return 0;
+       
+       if(k<=0)
+           return 0;
+       
+       if(node.val==targ.val)
+       {
+           
+           bfs(node.left,targ,k-1);
+           bfs(node.right,targ,k-1);
+           
+           return 1;
+       }
+       
+       int k1  = fn(node.left,targ,k);
+       
+       if(k1>=1)
+       {
+           if(k1==k)
+           res.add(node.val);
+           bfs(node.right,targ,k-k1-1);
+           
+           return k1+1;
+       }
+       
+       k1= fn(node.right,targ,k);
+       
+       if(k1>=1)
+       {
+           if(k1==k)
+           res.add(node.val);
+           bfs(node.left,targ,k-k1-1);
+           
+           return k1+1;
+       }
+       
+       return -1;
+   }  
+ 
+    public void bfs(TreeNode root,TreeNode target,int lev)
+    {
+        if(root==null)
+            return;
+        
+        if(lev==0)
+        {
+            res.add(root.val);
+        }
+       
+        bfs(root.left,target,lev-1);
+        bfs(root.right,target,lev-1);
+    }
+}
