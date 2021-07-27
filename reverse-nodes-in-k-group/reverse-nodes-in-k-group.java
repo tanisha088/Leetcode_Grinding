3bd4f9c424e173a91285cf7e60492caa2eab9081
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-27 11:15:46 +0530

    Time: 1 ms (27.22%), Space: 39.4 MB (40.50%) - LeetHub

--- reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -11,49 +11,85 @@
 class Solution {
     public ListNode reverseKGroup(ListNode head, int k) {
         
+        ListNode dummy =  new ListNode(head.val);
+        
+        dummy.next = head;
+        ListNode prev=dummy;
         ListNode st = head;
-        ListNode prev=null;
-        while(st!=null)          
+       
+        
+        while(st!=null && st.next!=null)
         {
-            int i=1;
-            ListNode s1 = st;
-            ListNode s2 = st;
-            
-            while(s1!=null && i<k)
+         int count = k;
+             ListNode then = st.next;
+            ListNode check = then;
+            int c=0;
+            while(check!=null && c<k-1)
             {
-                i++;
-            s1=s1.next;
+                check=check.next;
+                c=c+1;
             }
-             if(s1==null)
-                 break;
-            ListNode ta_last = s1.next;
-            s1.next=null;
-            if(prev!=null)
-                prev.next=null;
             
-          st = reverse(s2);
-            
-            if(prev==null)
-            {
-                head= st;
-            }
-            else      // st->3 , prev=1 , i=2 , s1 =2, s2=1 ,ta_last = 3 , s3 = 2 , head=2
-            {
-                prev.next = st;
-            }
-            i=1;
-             while(i<k)
-            {
-                i++;
-            st=st.next;
-            }
-            st.next=ta_last;
-            
-            prev= st;
+            if(c<k-1)
+                break;
+        while(count>1 && st!=null && then!=null)
+        {
+            st.next= then.next;
+            then.next = prev.next;
+            prev.next=then;
+            then  = st.next;
+            count--;
+        }
+         prev= st;
             st=st.next;
         }
         
-        return head;
+        return dummy.next;
+        
+        
+//         ListNode st = head;
+//         ListNode prev=null;
+//         while(st!=null)          
+//         {
+//             int i=1;
+//             ListNode s1 = st;
+//             ListNode s2 = st;
+            
+//             while(s1!=null && i<k)
+//             {
+//                 i++;
+//             s1=s1.next;
+//             }
+//              if(s1==null)
+//                  break;
+//             ListNode ta_last = s1.next;
+//             s1.next=null;
+//             if(prev!=null)
+//                 prev.next=null;
+            
+//           st = reverse(s2);
+            
+//             if(prev==null)
+//             {
+//                 head= st;
+//             }
+//             else      // st->3 , prev=1 , i=2 , s1 =2, s2=1 ,ta_last = 3 , s3 = 2 , head=2
+//             {
+//                 prev.next = st;
+//             }
+//             i=1;
+//              while(i<k)
+//             {
+//                 i++;
+//             st=st.next;
+//             }
+//             st.next=ta_last;
+            
+//             prev= st;
+//             st=st.next;
+//         }
+        
+//         return head;
     }
     
     public ListNode reverse(ListNode head)
