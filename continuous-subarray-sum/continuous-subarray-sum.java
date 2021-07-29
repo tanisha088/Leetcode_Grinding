f1e610b756cb1d61315b624bfd626e1d44c0960a
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-29 07:19:20 +0530

    Time: 15 ms (66.25%), Space: 55.4 MB (55.19%) - LeetHub

--- continuous-subarray-sum/continuous-subarray-sum.java
+++ continuous-subarray-sum/continuous-subarray-sum.java
@@ -1,27 +1,60 @@
 class Solution {
     public boolean checkSubarraySum(int[] nums, int k) {
-       HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
-       h.put(0,1);
-        int res=0;
+//        HashMap<Integer,Integer> h= new HashMap<Integer,Integer>();
+//        h.put(0,1);
+//         int res=0;
   
         
-        for(int i=0;i<nums.length;i++)
-        {
-            int prev= res;
-            res+=nums[i];
-            int val = res<0? (res%k+k)%k : res%k;
-            int prevv  = prev<0 ? prev%k+k : prev%k;
+//         for(int i=0;i<nums.length;i++)
+//         {
+//             int prev= res;
+//             res+=nums[i];
+//             int val = res<0? (res%k+k)%k : res%k;
+//             int prevv  = prev<0 ? prev%k+k : prev%k;
       
-            System.out.println(prevv+" "+val);
-            if(prevv==val && h.get(prevv)==1)
-            {
-                 h.put(val,h.getOrDefault(val,0)+1);
-                continue;
-            }
+         
+//             if(prevv==val && h.get(prevv)==1)
+//             {
+//                  h.put(val,h.getOrDefault(val,0)+1);
+//                 continue;
+//             }
         
-            if(h.containsKey(val))
+//             if(h.containsKey(val))
+//                 return true;
+//             h.put(val,h.getOrDefault(val,0)+1);
+//         }
+        
+//         return false;
+        
+        // 5 5 5
+        
+//         3 -10 -7     
+        
+//         -3 -10 -13    
+            
+        // a a+5 
+        
+        HashMap<Integer,Integer> h = new HashMap<Integer,Integer>();
+        
+        int res=0;
+        h.put(0,-1);
+        int c=0;
+        for(int i:nums)
+        {
+            res=res+i;
+            int val = (res%k+k)%k;
+           
+          
+             if(!h.containsKey(val))
+                  h.put(val,c);
+            else {
+                     int  m =h.get(val);
+    
+                if(c-m>=2)
                 return true;
-            h.put(val,h.getOrDefault(val,0)+1);
+            }
+                
+            c++;
         }
         
         return false;
