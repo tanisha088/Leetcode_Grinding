fbcc8ff7bc58b66d36e6642a800249f8ee8119d0
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-07-29 16:51:25 +0530

    Time: 4 ms (91.41%), Space: 42.1 MB (88.01%) - LeetHub

--- longest-turbulent-subarray/longest-turbulent-subarray.java
+++ longest-turbulent-subarray/longest-turbulent-subarray.java
@@ -1,6 +1,7 @@
 class Solution {
-    public int maxTurbulenceSize(int[] arr) {
+    public int maxTurbulenceSize(int[] A) {
         
+        /*
         int st=0;
         boolean fg = false;
         boolean fl = false;
@@ -64,6 +65,89 @@ class Solution {
         }
         ans = Math.max(ans,i-st+1);
         return ans;
+        
+        */
+        
+        /************************* 2ND APPROACH ********************************/
+        
+        
+//           int inc = 1, dec = 1, result = 1;
+//         for (int i = 1; i < A.length; i++) {
+//             if (A[i] < A[i - 1]) {
+//                 dec = inc + 1;
+//                 inc = 1;
+//             } else if (A[i] > A[i - 1]) {
+//                 inc = dec + 1;
+//                 dec = 1;
+//             } else {
+//                 inc = 1;
+//                 dec = 1;
+//             }
+            
+//             System.out.println(inc+" "+dec);
+//             result = Math.max(result, Math.max(dec, inc));
+//         }
+//         return result;
+    
+        
+        if(A.length<=1)
+            return A.length;
+        
+        int ans =1;
+        int i=0;
+        while(i<A.length-1)
+        {
+          if(A[i]==A[i+1])
+          {i++; continue;}
+             if(A[i]<A[i+1])
+            {
+                int st=i;
+                int c=0;
+                i++;
+                
+                while(i<A.length-1)
+                {
+                    if(c%2!=0)
+                       { 
+                        if(A[i]<A[i+1]){ i++; c++;}
+                    else { break;}}
+                    else 
+                      { if(A[i]>A[i+1]){ i++; c++;}
+                    else {break;}}
+                }
+                ans=Math.max(ans,i-st+1);
+                 
+            }
+            
+          
+          if(i>A.length-2)
+              break;
+             if(A[i]>A[i+1])
+            {
+                int st=i;
+                int c=0;
+                i++;
+                
+                while(i<A.length-1)
+                {
+                    
+                     if(c%2==0)
+                       { 
+                        if(A[i]<A[i+1]){ i++; c++;}
+                    else { break;}}
+                    else 
+                      { if(A[i]>A[i+1]){ i++; c++;}
+                    else {break;}}
+                }
+                ans=Math.max(ans,i-st+1);
+                
+            }
+                
+        }
+        
+        
+        return ans;
+        
     }
 }
 
