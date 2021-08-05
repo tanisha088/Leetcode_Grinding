bfc7f4da5c2009ffd3bea979429e01b9a91bb9ca
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-05 08:57:42 +0530

    Time: 1 ms (100.00%), Space: 39 MB (81.88%) - LeetHub

---
+++ valid-anagram/valid-anagram.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        
+        char[] arr = new char[26];
+        
+        for(char c:s.toCharArray())
+            arr[c-'a']+=1;
+        
+        for(char c:t.toCharArray())
+            arr[c-'a']-=1;
+        
+        for(int i=0;i<26;i++)
+            if(arr[i]!=0)
+                return false;
+        
+        return true;
+    }
+}
