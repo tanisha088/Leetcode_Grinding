077c8f20887e532551cc4656f3ebee81914db191
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-07 17:15:21 +0530

    Time: 0 ms (100.00%), Space: 36.6 MB (99.42%) - LeetHub

--- unique-paths-ii/unique-paths-ii.java
+++ unique-paths-ii/unique-paths-ii.java
@@ -5,45 +5,82 @@ class Solution {
         
         if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]==1)
             return 0;
+//         for(int i=0;i<obstacleGrid.length;i++)
+//         {
+//             for(int j=0;j<obstacleGrid[0].length;j++)
+//             {
+//                 if(i==0 && j==0)
+//                 {
+//                     obstacleGrid[i][j]=1;
+//                     continue;
+//                     }
+//                         if(obstacleGrid[i][j]==1)
+//                 {
+//                     obstacleGrid[i][j]=-1;
+//                     continue;
+//                 }   
+                
+//                 /*
+//                 if(i==0)
+//                 {
+//                     if(obstacleGrid[i][j-1]!=-1)
+//                         obstacleGrid[i][j]=obstacleGrid[i][j-1];
+//                 }
+                
+//                 else if(j==0)
+//                 {
+//                      if(obstacleGrid[i-1][j]!=-1)
+//                         obstacleGrid[i][j]=obstacleGrid[i-1][j];
+//                 }
+//                 else
+//                 {
+//                     if(obstacleGrid[i-1][j]!=-1)
+//                         obstacleGrid[i][j]+=obstacleGrid[i-1][j];
+                    
+//                     if(obstacleGrid[i][j-1]!=-1)
+//                         obstacleGrid[i][j]+=obstacleGrid[i][j-1];
+//                 }
+//                 */
+                
+//    // THE WHOLE ABOVE CONDITION SET CAN BE EASILY REPLACED BY THIS ----------->>>>>>>>
+//                     if(i>0)
+//                     if(obstacleGrid[i-1][j]!=-1)
+//                         obstacleGrid[i][j]+=obstacleGrid[i-1][j];
+                    
+//                 if(j>0)
+//                     if(obstacleGrid[i][j-1]!=-1)
+//                         obstacleGrid[i][j]+=obstacleGrid[i][j-1];
+                
+//             }
+//         }
+        
+//         return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        
+        
         for(int i=0;i<obstacleGrid.length;i++)
         {
             for(int j=0;j<obstacleGrid[0].length;j++)
             {
                 if(i==0 && j==0)
-                {
                     obstacleGrid[i][j]=1;
-                    continue;
-                    }
-                        if(obstacleGrid[i][j]==1)
+               else if(i==0 || j==0)
                 {
-                    obstacleGrid[i][j]=-1;
-                    continue;
-                }   
-                
-                if(i==0)
-                {
-                    if(obstacleGrid[i][j-1]!=-1)
-                        obstacleGrid[i][j]=obstacleGrid[i][j-1];
-                }
-                
-                else if(j==0)
-                {
-                     if(obstacleGrid[i-1][j]!=-1)
-                        obstacleGrid[i][j]=obstacleGrid[i-1][j];
+                    if(obstacleGrid[i][j]!=1)
+                        obstacleGrid[i][j]=((i==0)?obstacleGrid[i][j-1]:obstacleGrid[i-1][j]);
+                    else
+                        obstacleGrid[i][j]=0;
                 }
                 else
                 {
-                    if(obstacleGrid[i-1][j]!=-1)
-                        obstacleGrid[i][j]+=obstacleGrid[i-1][j];
-                    
-                    if(obstacleGrid[i][j-1]!=-1)
-                        obstacleGrid[i][j]+=obstacleGrid[i][j-1];
+                    if(obstacleGrid[i][j]==1)
+                        obstacleGrid[i][j]=0;
+                    else
+                    obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
                 }
-                
-                
             }
         }
         
         return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        
     }
 }
