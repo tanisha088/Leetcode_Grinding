50d514f6fdc4d32b664aed7b9fd3de2d55d09440
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-11 19:43:47 +0530

    Time: 5 ms (56.72%), Space: 39.4 MB (24.63%) - LeetHub

--- edit-distance/edit-distance.java
+++ edit-distance/edit-distance.java
@@ -1,24 +1,74 @@
 class Solution {
     public int minDistance(String word1, String word2) {
-        int[][] dp= new int[word1.length()+1][word2.length()+1];
+//         int[][] dp= new int[word1.length()+1][word2.length()+1];
+//         for(int i=0;i<word1.length()+1;i++)
+//         {
+//             for(int j=0;j<word2.length()+1;j++)
+//             {
+//                 // V,.V..V.V..V. IMP 
+//                 if(i==0 || j==0)
+//                 {
+//                     dp[i][j]=(i==0?j:i);
+//                     continue;
+//                 }
+//                 if(word1.charAt(i-1)==word2.charAt(j-1))
+//                     dp[i][j]=dp[i-1][j-1];
+//                 else
+//                 {
+//                     dp[i][j]=Math.min(dp[i][j-1]+1,Math.min(dp[i-1][j-1]+1,dp[i-1][j]+1));
+//                 }
+//             }
+//         }
+        
+//         return dp[word1.length()][word2.length()];
+        
+        
+        
+        
+        int[][] ans=  new int[word1.length()+1][word2.length()+1];
+        
         for(int i=0;i<word1.length()+1;i++)
         {
-            for(int j=0;j<word2.length()+1;j++)
+            ans[i][0]=i;
+        }
+        
+        for(int j=0;j<=word2.length();j++)
+        {
+            ans[0][j]=j;
+        }
+        
+        for(int i=1;i<=word1.length();i++)
+        {
+            for(int j=1;j<=word2.length();j++)
             {
-                if(i==0 || j==0)
+                if(word1.charAt(i-1)!=word2.charAt(j-1))
                 {
-                    dp[i][j]=(i==0?j:i);
-                    continue;
+                    ans[i][j]= 1+Math.min(ans[i-1][j],Math.min(ans[i-1][j-1],ans[i][j-1]));
                 }
-                if(word1.charAt(i-1)==word2.charAt(j-1))
-                    dp[i][j]=dp[i-1][j-1];
-                else
-                {
-                    dp[i][j]=Math.min(dp[i][j-1]+1,Math.min(dp[i-1][j-1]+1,dp[i-1][j]+1));
-                }
+                else ans[i][j] = ans[i-1][j-1];
             }
         }
         
-        return dp[word1.length()][word2.length()];
+        return ans[word1.length()][word2.length()];
+
+    /*
+    
+     eyre
+       mye
+    */
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
     }
 }
