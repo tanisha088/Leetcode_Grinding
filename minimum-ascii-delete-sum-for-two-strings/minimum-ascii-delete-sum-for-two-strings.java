d35534b4d9e390631fdadc9b2ac1c0d01f5e5ab8
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-11 20:13:31 +0530

    Time: 18 ms (68.60%), Space: 39.6 MB (53.39%) - LeetHub

--- minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
+++ minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.java
@@ -1,20 +1,48 @@
 class Solution {
-    public int minimumDeleteSum(String st1, String st2) {
-        char[] s1 = st1.toCharArray();
-    char[] s2 = st2.toCharArray();
+    public int minimumDeleteSum(String str1, String str2) {
+        char[] s1 = str1.toCharArray();
+    char[] s2 = str2.toCharArray();
       int n1 = s1.length, n2 = s2.length;
         int[][] dp = new int[n1+1][n2+1] ;
-        for (int i1 = 1; i1 <=n1; ++i1) for (int i2 = 1; i2 <=n2; ++i2)  {
-            int ans = 0;
-            if(s1[i1-1]!=s2[i2-1])
-            ans = Math.max(dp[i1][i2-1], dp[i1-1][i2]);
-            else ans = Math.max(ans, (int)s1[i1-1] + dp[i1-1][i2-1]);
-            dp[i1][i2] = ans;
+        // String[][] arr= new String[n1+1][n2+1];
+        // for (int i1 = 1; i1 <=n1; ++i1) for (int i2 = 1; i2 <=n2; ++i2)  {
+        //     int ans = 0;
+        //     if(s1[i1-1]!=s2[i2-1])
+        //     ans = Math.max(dp[i1][i2-1], dp[i1-1][i2]);
+        //     else ans = Math.max(ans, (int)s1[i1-1] + dp[i1-1][i2-1]);
+        //     dp[i1][i2] = ans;
+        // }
+        // int ret = 0;
+        // for (int c:s1) ret +=c;
+        // for (int c:s2) ret +=c;
+        // ret -= 2*dp[n1][n2];
+        // return ret;   
+        
+        for(int i=1;i<=n1;i++)
+        {
+            dp[i][0] = (int)str1.charAt(i-1)+dp[i-1][0];
         }
-        int ret = 0;
-        for (int c:s1) ret +=c;
-        for (int c:s2) ret +=c;
-        ret -= 2*dp[n1][n2];
-        return ret;   
+        
+        for(int j=1;j<=n2;j++)
+        {
+            dp[0][j]=(int)str2.charAt(j-1) + dp[0][j-1];
+        }
+        
+        for(int i=1;i<=n1;i++)
+        {
+            for(int j=1;j<=n2;j++)
+            {
+                if(str1.charAt(i-1)==str2.charAt(j-1))
+                    dp[i][j]= dp[i-1][j-1] ;
+                else
+                {
+                    dp[i][j] = Math.min(dp[i-1][j]+(int)str1.charAt(i-1),dp[i][j-1]+(int)str2.charAt(j-1));
+                }
+            }
+        }
+        
+        
+        return dp[str1.length()][str2.length()];
+        
     }
 }
