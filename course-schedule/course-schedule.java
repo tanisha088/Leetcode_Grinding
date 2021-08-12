9331dd157bd4390197de1c728666b9da7a327772
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-12 08:58:14 +0530

    Time: 3 ms (85.10%), Space: 40.2 MB (23.41%) - LeetHub

--- course-schedule/course-schedule.java
+++ course-schedule/course-schedule.java
@@ -16,45 +16,80 @@ class Solution {
                 
         }
       
-        HashSet<Integer> visi= new HashSet<Integer>();
-        HashSet<Integer> h= new HashSet<Integer>();
+//         HashSet<Integer> visi= new HashSet<Integer>();
+//         HashSet<Integer> h= new HashSet<Integer>();
         
+//         for(int i=0;i<numCourses;i++)
+//         {
+//      //      System.out.println(i+" "+h.contains(i));
+//         if(!h.contains(i))
+//         if(!dfs(i,list,h,visi))
+//             return false;
+//         }
+        
+//         return true;
+//     }
+    
+//     public boolean dfs(int src, ArrayList<ArrayList<Integer>> adj,HashSet<Integer> visi,HashSet<Integer> h)
+//     {
+//         if(visi.contains(src))
+//             return false;
+        
+//         visi.add(src);
+        
+//         if(h.contains(src))
+//             return true;
+        
+          
+//         for(int i=0;i<adj.get(src).size();i++)
+//         {
+//             if(h.contains(adj.get(src).get(i)))
+//                 continue;
+            
+            
+//             boolean k = dfs(adj.get(src).get(i),adj,visi,h);
+//             if(!k)
+//                 return false;
+            
+//         }
+        
+        
+//         h.add(src);
+//         return true;
+//     }
+        
+        HashSet<Integer> set = new HashSet();
         for(int i=0;i<numCourses;i++)
         {
-     //      System.out.println(i+" "+h.contains(i));
-        if(!h.contains(i))
-        if(!dfs(i,list,h,visi))
-            return false;
+              HashSet<Integer> visi = new HashSet();
+            if(!set.contains(i))
+                if(dfs(i,list,visi,set))
+                    return false;
         }
         
         return true;
     }
     
-    public boolean dfs(int src, ArrayList<ArrayList<Integer>> adj,HashSet<Integer> visi,HashSet<Integer> h)
+    public static boolean dfs(int src,ArrayList<ArrayList<Integer>> list,HashSet<Integer> visited,HashSet<Integer> set)
     {
-        if(visi.contains(src))
-            return false;
-        
-        visi.add(src);
-        
-        if(h.contains(src))
+        if(visited.contains(src))
             return true;
         
-          
-        for(int i=0;i<adj.get(src).size();i++)
+        visited.add(src);
+        
+        for(int i=0;i<list.get(src).size();i++)
         {
-            if(h.contains(adj.get(src).get(i)))
-                continue;
-            
-            
-            boolean k = dfs(adj.get(src).get(i),adj,visi,h);
-            if(!k)
-                return false;
-            
+            if(!set.contains(list.get(src).get(i)))
+            {
+            boolean k=  dfs(list.get(src).get(i),list,visited,set);
+            if(k)
+                return true;
+            }
         }
         
+        set.add(src);
+        visited.remove(src);
         
-        h.add(src);
-        return true;
+        return false;
     }
 }
