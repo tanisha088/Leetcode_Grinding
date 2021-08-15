ea5ad7cbfb2e4659a10d87f1a1ccff65621d48b9
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-15 19:16:45 +0530

    Time: 2 ms (99.96%), Space: 47.8 MB (14.07%) - LeetHub

--- number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
+++ number-of-subarrays-with-bounded-maximum/number-of-subarrays-with-bounded-maximum.java
@@ -1,44 +1,99 @@
 class Solution {
     public int numSubarrayBoundedMax(int[] A, int L, int R) {
 
-          int sum1 =1;
-        int sum2=1;
-        int ans=0;
-        for(int i=0;i<A.length;i++)
-        {
-            if(A[i]>R)
-                sum1=0;
+//           int sum1 =1;
+//         int sum2=1;
+//         int ans=0;
+//         for(int i=0;i<A.length;i++)
+//         {
+//             if(A[i]>R)
+//                 sum1=0;
           
-                ans=ans+sum1;
-            sum1+=1;
+//                 ans=ans+sum1;
+//             sum1+=1;
         
-            if(A[i]>=L)
-                sum2=0;
+//             if(A[i]>=L)
+//                 sum2=0;
             
-            ans=ans-sum2;
-            sum2+=1;
-        //    System.out.println(ans+" "+i+" "+sum1);
-        }
+//             ans=ans-sum2;
+//             sum2+=1;
+//         //    System.out.println(ans+" "+i+" "+sum1);
+//         }
         
-        /*
-int ans2=0;        
-         for(int i=0;i<A.length;i++)
-        {
-            if(A[i]>=L)
-            {
-                ans2+=sum2;
-                sum2=0;
-                continue;
-            }
-            else
-                sum2= sum2 + sum2+ 1;
-              if(i==A.length-1)
-                ans2+=sum2;
+//         /*
+// int ans2=0;        
+//          for(int i=0;i<A.length;i++)
+//         {
+//             if(A[i]>=L)
+//             {
+//                 ans2+=sum2;
+//                 sum2=0;
+//                 continue;
+//             }
+//             else
+//                 sum2= sum2 + sum2+ 1;
+//               if(i==A.length-1)
+//                 ans2+=sum2;
                 
+//         }
+//         */
+//      System.out.println(ans);
+//         return ans;
+        
+//         int ans=0;
+//         int count=0;
+//         int i=0;
+//         int max=Integer.MIN_VALUE;
+//         while(i<A.length)
+//         {
+//             max=Math.max(A[i],max);
+//             if(max<=R && max>=L)
+//             {
+//                  if(max==A[i])
+//                 count++;
+                
+//                 ans = ans + count;
+               
+//             }
+//             else
+//             {
+//                 max=Integer.MIN_VALUE;
+//                 count=0;
+//             }
+//             i++;
+//         }
+                                                                                            
+//         return ans;
+        
+        int i=0;
+        int ans=0;
+        int s1=1;
+        int s2=1;
+    while(i<A.length)
+    {
+        if(A[i]>R)
+        {
+            s1=1;
+            s2=1;
+            i++;
+            continue;
         }
-        */
-     System.out.println(ans);
+      
+        ans = ans + s1;
+        s1++;
+        
+        if(A[i]>=L)
+            s2=0;
+        ans =  ans - s2;
+        s2++;
+
+       i++;
+
+    }
+        
         return ans;
+        
+        
     }
 }
 
