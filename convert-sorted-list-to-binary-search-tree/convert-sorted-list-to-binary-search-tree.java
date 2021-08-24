afcb26e5488c3d652ab31b08e4827d3f594687cf
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-24 05:40:38 +0530

    Time: 0 ms (100.00%), Space: 40.1 MB (48.84%) - LeetHub

--- convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
+++ convert-sorted-list-to-binary-search-tree/convert-sorted-list-to-binary-search-tree.java
@@ -27,6 +27,7 @@ class Solution {
     ListNode gh= null;
     public TreeNode sortedListToBST(ListNode head) {
         
+        /*
         int size=0;
         ListNode h = head;
         while(h!=null)
@@ -36,8 +37,60 @@ class Solution {
         }
         gh = head;
         return stobst(size);
+        */
+        
+        // INCASE YOU DON'T WANT TO KEEP A GLOBAL HEAD ---->
+    //      if(head==null) return null;
+    // return toBST(head,null);
+        
+        if(head==null)
+            return null;
+        
+        if(head.next==null)
+	return new TreeNode(head.val);
+
+	
+	ListNode prev=null;
+
+	ListNode slow=head;
+	ListNode fast=head;
+
+	while(fast!=null && fast.next!=null)
+	{
+	prev=slow;
+	slow=slow.next;
+	fast=fast.next.next;
+	}
+
+    TreeNode node = new TreeNode(slow.val);
+
+prev.next= null;
+ListNode next = slow.next;
+slow.next= null;
+    //    System.out.println(node.val);
+    node.left = sortedListToBST(head);
+    node.right = sortedListToBST(next);
+
+return node;
+        
     }
     
+   
+     
+public TreeNode toBST(ListNode head, ListNode tail){
+    ListNode slow = head;
+    ListNode fast = head;
+    if(head==tail) return null;
+    
+    while(fast!=tail&&fast.next!=tail){
+        fast = fast.next.next;
+        slow = slow.next;
+    }
+    TreeNode thead = new TreeNode(slow.val);
+    thead.left = toBST(head,slow);
+    thead.right = toBST(slow.next,tail);
+    return thead;
+}
     public TreeNode stobst(int size)
     {
         if(size==0)
