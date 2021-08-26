66e2f0a6ee6cc98c046d0a90683771803ad9f2ac
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-26 12:21:03 +0530

    Time: 893 ms (5.00%), Space: 64.8 MB (5.76%) - LeetHub

--- accounts-merge/accounts-merge.java
+++ accounts-merge/accounts-merge.java
@@ -1,79 +1,84 @@
 class Solution {
     public List<List<String>> accountsMerge(List<List<String>> accounts) {
-     
-        HashMap<String,Integer> h= new HashMap<String,Integer>();
-        HashMap<Integer,Integer> par= new HashMap<Integer,Integer>();
-        int tot=accounts.size();
+           HashMap<String,HashSet<Integer>> h = new HashMap();
+        
+        int[] parents = new int[accounts.size()];
+        for(int i=0;i<parents.length;i++)
+            parents[i]=i;
+        
         for(int i=0;i<accounts.size();i++)
         {
-            par.put(i,i);
-            boolean u=false;
-            List<String> list= accounts.get(i);
-            for(int j=1;j<list.size();j++)
+            List<String> l1 = accounts.get(i);
+            for(int j=1;j<l1.size();j++)
             {
-                if(h.containsKey(list.get(j)) && h.get(list.get(j))!=i)
-                {
-                    int a1 = find(h.get(list.get(j)),par);
-                    int b1 = find(i,par);
-                    par.put(a1,b1);
-                    if(a1!=b1)
-                        tot--;
-                    u=true;
-                }
+                if(!h.containsKey(l1.get(j)))
+                    h.put(l1.get(j),new HashSet());
+              
+                HashSet<Integer> set = h.get(l1.get(j));
+                if(set.size()==0)
+                    set.add(i);
                 else
-                    h.put(list.get(j),i);
+                {
+                    int m =0;
+                    for(int j1:set)
+                        m=j1;
+                    
+                    int pa = find(parents,i);
+                    int pb = find(parents,m);
+                    parents[pa]=pb;
+                    
+                
+                }
+                
+                h.put(l1.get(j),set);
+                
             }
         }
         
-        List<List<String>> result= new ArrayList<List<String>>();
-        for(int i=0;i<accounts.size();i++)
+   //     System.out.println(h);
+        
+        List<List<String>> list=  new ArrayList();
+        HashMap<Integer,List<String>> map = new HashMap();
+   
+        for(int i=0;i<parents.length;i++)
         {
-            result.add(new ArrayList<String>());
-        }
-      
-        for(int i=0;i<accounts.size();i++)
-        {
-            int m = find(i,par);
-            if(result.get(m).size()==0)
-                result.get(m).add(accounts.get(i).get(0));
+            int par = find(parents,i);
+            List<String> ll = map.getOrDefault(par,new ArrayList());
+
             for(int j=1;j<accounts.get(i).size();j++)
             {
-                if(!result.get(m).contains(accounts.get(i).get(j)))
-                 result.get(m).add(accounts.get(i).get(j));
+                if(!ll.contains(accounts.get(i).get(j)))
+                    ll.add(accounts.get(i).get(j));
             }
-         
+            
+            map.put(par,ll);
         }
+          //   System.out.println(map);
         
-        for(int i=0;i<result.size();i++)
+        for(Map.Entry<Integer,List<String>> e:map.entrySet())
         {
-            ArrayList<String> a2 = new ArrayList<String>();
-            if(result.get(i).size()==0)
-                continue;
-            a2.add(result.get(i).get(0));
-            result.get(i).remove(0);
-            Collections.sort(result.get(i));
-            for(int j=0;j<result.get(i).size();j++)
-                a2.add(result.get(i).get(j));
-            result.set(i,a2); 
+            List<String> ll = e.getValue();
+            
+            Collections.sort(ll);
+                      
+                String name =  accounts.get(e.getKey()).get(0);
+                ll.add(0,name);
+    
+            list.add(ll);
         }
-    int i=0;
         
-         while(result.size()!=tot)
-         {
-            // System.out.println(result+" "+i+" "+tot);
-             if(result.get(i).size()>0)
-                 i++;
-             else
-                 result.remove(i);
-         }
-        return result;
+        return list;
+        
     }
     
-    public int find(int a,HashMap<Integer,Integer> par)
+    public int find(int[] parents,int v)
     {
-        if(par.get(a)==a)
-            return a;
+        if(parents[v]==v)
+            return v;
         
-        return find(par.get(a),par);
+        int m =  find(parents,parents[v]);
+        parents[v] =  m;
+        
+        return m;
     }
 }
