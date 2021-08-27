135abbecbe4d96cd550d815c385fb4650957c4b3
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-27 12:18:52 +0530

    Time: 3 ms (50.11%), Space: 46 MB (12.05%) - LeetHub

--- jump-game-ii/jump-game-ii.java
+++ jump-game-ii/jump-game-ii.java
@@ -1,6 +1,7 @@
 class Solution {
     public int jump(int[] nums) {
        
+        /*
         for(int i=nums.length-2;i>=0;i--)
         {
             int minval=nums.length;
@@ -18,6 +19,73 @@ class Solution {
         if(nums.length==1)
             return 0;
         return nums[0];
+     */
+        
+      //  GREEDY ::::
+        
+//         int curmax=0;
+//         int nextmax=0;
+//         int jump=0;
+//         for(int i=0;i<nums.length-1;i++)
+//         {
+//             nextmax=Math.max(nextmax,nums[i]+i);
+//             if(curmax==i)
+//             {
+//                 curmax=nextmax;
+//                 jump++;
+//             }
+//         }
+        
+//         return jump;
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+ int curmax=0;
+        int totmax=0;
+        int count=0;
+        int i=0;
+        while(true)
+        {
+            
+         //   System.out.println(curmax+" "+totmax+" "+count+" "+i);
+            if(curmax>=nums.length-1)
+                return count;
+            
+            if(curmax<i)
+            {
+                curmax = totmax;
+                count++;
+            }
+            
+            totmax = Math.max(totmax,i+nums[i]);
+            i++;
+        }
+        
+        
+//         2-3-1-1-4
+        
+//        curmax >= arr.length-1 -> return count
+        
+//        totmax -> curmax &  count++  
+            
+//             totmax= update 
+        
+        
         
     }
 }
