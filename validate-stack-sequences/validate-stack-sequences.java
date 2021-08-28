ac261fd6fa79fc4f154c9294846eadc7c01e74fe
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-28 08:53:15 +0530

    Time: 7 ms (7.35%), Space: 42.2 MB (10.63%) - LeetHub

--- validate-stack-sequences/validate-stack-sequences.java
+++ validate-stack-sequences/validate-stack-sequences.java
@@ -1,31 +1,72 @@
 class Solution {
     public boolean validateStackSequences(int[] pushed, int[] popped) {
         Stack<Integer> st=new Stack<Integer>();
+        
         int i=0;
         int j=0;
         
-        while(j<popped.length && i<pushed.length)
+       
+        while(i<popped.length)
         {
-            if(st.isEmpty() || st.peek()!=popped[j])
-            {
-                st.push(pushed[i]);
-                i++;
-            }
-            else 
-            {
-                st.pop();
-                j++;
-            }
-        }
+      while(!st.isEmpty() && i<popped.length && st.peek()==popped[i])
+{
+     st.pop();
+     i++;
+
+}
+            if(j==pushed.length)
+                break;
+     while(j<pushed.length)
+           {
+         
+         
+           st.push(pushed[j]);
+         if(pushed[j]==popped[i])
+             {j++; break;}
+             j++;
+
         
-        while(j<popped.length)
-        {
-            if(st.isEmpty() || st.peek()!=popped[j] )
-                return false;
-            j++;
-            st.pop();
+           }
         }
+
+return st.isEmpty()==true;
+
         
-        return true;
+
     }
 }
+
+/*  1 2 3 4 5 */
+
+
+/*
+
+while(i<popped.length)
+{
+
+if(!empty && st.peek()==popped[i])
+{
+     st.pop();
+     i++;
+
+}
+     while(j<pushed.length)
+           {
+           
+           st.push(pushed[j]);
+             j++;
+           if(pushed[j]==popped[i])
+           {
+         
+            break;
+           }
+           }
+           
+           
+}
+
+
+return st.isEmpty()==true?
+
+
+*/
