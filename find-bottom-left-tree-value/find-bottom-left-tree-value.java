abe9e17d8db1fe4a8988f36a82b8677ed516aa0c
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-30 22:01:26 +0530

    Time: 1 ms (68.18%), Space: 41.3 MB (5.65%) - LeetHub

--- find-bottom-left-tree-value/find-bottom-left-tree-value.java
+++ find-bottom-left-tree-value/find-bottom-left-tree-value.java
@@ -14,31 +14,126 @@
  * }
  */
 class Solution {
-    TreeNode val = null;
+    
+    
+    int ans;
+    int k;
+    
     public int findBottomLeftValue(TreeNode root) {
         
-        int height = h(root);
-        lot(root,height);
+        // 1ST APPROACH --->
+     //   int height = h(root);
+     //   lot(root,height);
+     // return val.val;
         
-        return val.val;
+        // 2ND APPROACH ---> 
+//         int[] res= new int[2];
+//         res[0]=0;
+//         res[1]=-1;
+//         inorder(root,0,res);
+        
+//         return res[0];
+        
+        ans=0;
+        k=-1;
+        
+        fn(root,0);
+        
+        return ans;
     }
     
-    public int h(TreeNode root)
-    {
-        if(root==null)
-            return 0;
-        
-        return 1+ Math.max(h(root.left),h(root.right));
-    }
-    public void lot(TreeNode root,int level)
+    public void fn(TreeNode root,int lev)
     {
         if(root==null)
             return;
         
-        if(level==1 && val==null)
-            val=root;
+        fn(root.left,lev+1);
         
-        lot(root.left,level-1);
-        lot(root.right,level-1);
+        if(lev>k)
+        {
+            ans = root.val;
+            k = lev;
+        }
+        
+        fn(root.right,lev+1);
     }
+    
+    
+    
+//     public void inorder(TreeNode root,int level,int[] res)
+//     {
+//         if(root==null)
+//             return;
+        
+//           if(level>res[1])
+//         {
+//             res[0]=root.val;
+//             res[1]=level;
+//         }
+        
+//         inorder(root.left,level+1,res);
+//         inorder(root.right,level+1,res);
+        
+      
+//     }
+    
+//     public int h(TreeNode root)
+//     {
+//         if(root==null)
+//             return 0;
+        
+//         return 1+ Math.max(h(root.left),h(root.right));
+//     }
+//     public void lot(TreeNode root,int level)
+//     {
+//         if(root==null)
+//             return;
+        
+//         if(level==1 && val==null)
+//             val=root;
+        
+//         lot(root.left,level-1);
+//         lot(root.right,level-1);
+//     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//     5
+//    / \ 
+//    3  4
+//      /\
+//      2 1
+    
+        
+//         k , ans
+        
+//         5,0
+//         3,1
+        
+//         if(param>k)
+//              ans=node.val
+
+
+
+
+
+
+
+
+
+
+
+
+
