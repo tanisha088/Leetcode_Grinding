935d7e311dbca6366a224443f10b1a55d6973eba
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-08-30 19:03:22 +0530

    Time: 37 ms (86.10%), Space: 49.7 MB (70.21%) - LeetHub

---
+++ design-add-and-search-words-data-structure/design-add-and-search-words-data-structure.java
@@ -0,0 +1,81 @@
+class WordDictionary {
+
+    /** Initialize your data structure here. */
+    
+    class Node
+    {
+        int val;
+        Node[] arr;
+        
+        Node(int val)
+        {
+            this.val = val;
+            this.arr = new Node[26];
+        }
+    }
+    
+    
+    Node root;
+    public WordDictionary() {
+        root=null;
+    }
+    
+    public void addWord(String word) {
+        if(root==null)
+            root =  new Node(-1);
+        Node node =  root;
+     //   System.out.println(null==null?"yhuhuadsh":"bhaggg");
+        for(int i=0;i<word.length();i++)
+        {
+            // System.out.println(node.arr);.length +" " + (word.charAt(i)-'a'));
+            // Node m =  node.arr[word.charAt(i)-'a'];
+            if((node.arr[word.charAt(i)-'a'])==null)
+                node.arr[word.charAt(i)-'a'] = new Node(0);
+            node = node.arr[word.charAt(i)-'a'];
+        }
+        
+        node.val = 1;
+        
+    }
+    
+    public boolean search(String word) {
+        
+        Node node = root;
+        return find(word,0,node);
+        
+    }
+    public boolean find(String word,int i,Node node)
+    {
+        if(node==null)
+            return false;
+        
+        if(i==word.length())
+        {
+            if(node.val==1)
+            return true;
+            
+            return false;
+        }
+        
+      
+        if(word.charAt(i)!='.')
+        {
+            return find(word,i+1,node.arr[word.charAt(i)-'a']);
+        }
+        
+        for(int j=0;j<26;j++)
+        {
+            if(find(word,i+1,node.arr[j])==true)
+                return true;
+        }
+        
+        return false;
+    }
+}
+
+/**
+ * Your WordDictionary object will be instantiated and called as such:
+ * WordDictionary obj = new WordDictionary();
+ * obj.addWord(word);
+ * boolean param_2 = obj.search(word);
+ */
