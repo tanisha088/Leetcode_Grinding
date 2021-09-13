584f45e7761b8f9377db686c33d546f2f221f48d
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-09-13 18:07:59 +0530

    Time: 7 ms (5.64%), Space: 37.3 MB (21.94%) - LeetHub

--- reverse-linked-list-ii/reverse-linked-list-ii.java
+++ reverse-linked-list-ii/reverse-linked-list-ii.java
@@ -11,77 +11,86 @@
 class Solution {
     ListNode extra = null;
     public ListNode reverseBetween(ListNode head, int left, int right) {
-         if(head==null || head.next==null || left==right)
-             return head;
-       /* 
-         ListNode h2 = head;
-        int l=-1;
-        int r=-1;
-      while(h2!=null)
-      {
-          if(h2.val==left)
-              l++;
-          if(h2.val==right);
-          r++;
-          h2=h2.next;
-      }
-        if(l==-1 || r==-1)
+          
+        if(head==null || head.next==null)
             return head;
-        */
-        ListNode h1 = head;
-        ListNode prev=null;
-        int k=-1;
-        while(h1!=null && h1.next!=null && left>2)
+        
+        ListNode dummy =  new ListNode(-1,head);
+        
+        head=dummy;
+        int count = right-left;
+        while(left!=1)
         {
-             prev=h1;
-             h1=h1.next;
             left--;
-            right--;
-        }       int val = head.val;
-      if(prev!=null)
-             val=h1.next.val;
-        else
-        {
-            if(left==2)
-                val=head.next.val;
+            head=head.next;
         }
-        ListNode nh = reverse(h1,left,right-1,0);
-        if(nh==null)
-            return head;
+        ListNode prev = head;
+        ListNode curr = head.next;
+        if(curr.next==null)
+            return dummy.next;
+        ListNode then =  curr.next;
+   System.out.println(prev.val+" "+curr.val+" "+then.val);
+      while(count!=0)
+      {
+          ListNode pnext= prev.next;
+          prev.next = then;
+          curr.next = then.next;
+          then.next = pnext;
+          then = curr.next;
+          count--;
+      }
         
-     //   System.out.println(nh.val+" "+prev);
-        if(prev==null)
-            return nh;
-         prev.next=nh;
-        
-        return head;
+        return dummy.next;
     }
+        
+//          if(head==null || head.next==null || left==right)
+//              return head;
+  
+//         ListNode h1 = head;
+//         ListNode prev=null;
+//         int k=-1;
+//         while(h1!=null && h1.next!=null && left>2)
+//         {
+//              prev=h1;
+//              h1=h1.next;
+//             left--;
+//             right--;
+//         }      
+        
+//         ListNode nh = reverse(h1,left,right,0);
+      
+//         if(prev==null)
+//             return nh;
+//          prev.next=nh;
+        
+//         return head;
+//     }
     
-    public ListNode reverse(ListNode h1,int left,int right,int count)
-    {
-          if(right==0)
-          {
-              extra = h1.next;
-              h1.next=null;
-          }
+//     public ListNode reverse(ListNode h1,int left,int right,int count)
+//     {
+//           if(right==1)
+//           {
+//               extra = h1.next;
+//               h1.next=null;
+//           }
         
-        if(h1==null || h1.next==null)
-            return h1;
+//         if(h1==null || h1.next==null)
+//             return h1;
         
-        ListNode newHead = reverse(h1.next,left,right-1, count+1);
-        if(left!=1 && count==0)
-        {
-            h1.next.next=extra;
-            h1.next=newHead;
-            return h1;
-        }
+//         ListNode newHead = reverse(h1.next,left,right-1, count+1);
+//         if(left!=1 && count==0)
+//         {
+//             h1.next.next=extra;
+//             h1.next=newHead;
+//             return h1;
+//         }
         
-        h1.next.next=h1;
-        h1.next=null;
-        if(count==0 && left==1)
-            h1.next=extra;
-        return newHead;
-    }
+//         h1.next.next=h1;
+//         h1.next=null;
+//         if(count==0 && left==1)
+//             h1.next=extra;
+//         return newHead;
+//     }
 }
 
 // 1 -> 8 -> 9 -> 10 -> 2 -> 3 -> 4 -> 5 -> 11 -> 12-> 13
