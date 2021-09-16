cbe5751948ac5cb404df0c8b99ad1ef4b32292f1
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-09-16 16:11:53 +0530

    Time: 2 ms (27.09%), Space: 52 MB (16.73%) - LeetHub

--- coloring-a-border/coloring-a-border.java
+++ coloring-a-border/coloring-a-border.java
@@ -10,38 +10,88 @@ class Solution {
     
     public void dfs(int sr,int sc,int newcolor, int[][] image,boolean[][] visited,int val)
     {
-       
+        visited[sr][sc] = true;
         
-        visited[sr][sc]=true;
+        int[] x= {-1,0,1,0};
+        int[] y= {0,1,0,-1};
         
-        
-        
-        int[] x={-1,0,0,1};
-        int[] y={0,-1,1,0};
-        
-         if(sr==0 || sr==image.length-1 || sc==0 || sc==image[0].length-1)
-             image[sr][sc]=newcolor;
-        
-             for(int i=0;i<4;i++)
-        {
-            int nx=x[i]+sr;
-            int ny=y[i]+sc;
-            
-                 if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && image[nx][ny]!=val && !visited[nx][ny])
-                 {
-                     if(sr==2 && sc==4)
-                         System.out.println(nx+" "+ny);
-                     image[sr][sc]=newcolor;
-                 }
-        }
+        int count=0;
         
         for(int i=0;i<4;i++)
         {
-            int nx=x[i]+sr;
-            int ny=y[i]+sc;
+            int nx= x[i]+sr;
+            int ny= y[i]+sc;
             
-            if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && visited[nx][ny]==false && image[nx][ny]==val)
-                dfs(nx,ny,newcolor,image,visited,val);
+            if(nx>=0 && ny>=0 && nx<image.length && ny<image[0].length)
+            {
+                   if(!visited[nx][ny] && image[nx][ny]!=val)
+                       count++;
+                   
+                   if(!visited[nx][ny] && image[nx][ny]==val)
+                    dfs(nx,ny,newcolor,image,visited,val);
+                
+            }
+            
         }
-    }
+//System.out.println(sr+" "+sc+" "+count);        
+        if(count!=0 || (sr==0 || sc==0 || sr==image.length-1 || sc==image[0].length-1))
+        {
+            image[sr][sc]=newcolor;
+        }
+    } 
+        
+//         visited[sr][sc]=true;    
+//         int[] x={-1,0,0,1};
+//         int[] y={0,-1,1,0}; 
+//          if(sr==0 || sr==image.length-1 || sc==0 || sc==image[0].length-1)
+//              image[sr][sc]=newcolor; 
+//              for(int i=0;i<4;i++)
+//         {
+//             int nx=x[i]+sr;
+//             int ny=y[i]+sc;
+            
+//                  if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && image[nx][ny]!=val && !visited[nx][ny])
+//                      image[sr][sc]=newcolor;
+//         }
+        
+//         for(int i=0;i<4;i++)
+//         {
+//             int nx=x[i]+sr;
+//             int ny=y[i]+sc;
+            
+//             if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && visited[nx][ny]==false && image[nx][ny]==val)
+//                 dfs(nx,ny,newcolor,image,visited,val);
+//         }
+//     }
+        //   visited[sr][sc]=true;
+        
+        
+        
+//         int[] x={-1,0,0,1};
+//         int[] y={0,-1,1,0};
+        
+//          if(sr==0 || sr==image.length-1 || sc==0 || sc==image[0].length-1)
+//              image[sr][sc]=newcolor;
+        
+//              for(int i=0;i<4;i++)
+//         {
+//             int nx=x[i]+sr;
+//             int ny=y[i]+sc;
+            
+//                  if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && image[nx][ny]!=val && !visited[nx][ny])
+//                      image[sr][sc]=newcolor;
+//         }
+        
+//         for(int i=0;i<4;i++)
+//         {
+//             int nx=x[i]+sr;
+//             int ny=y[i]+sc;
+            
+//             if(nx>=0 && nx<image.length && ny>=0 && ny<image[0].length && visited[nx][ny]==false && image[nx][ny]==val)
+//                 dfs(nx,ny,newcolor,image,visited,val);
+//         }
+        
+        
+        
+   // }
 }
