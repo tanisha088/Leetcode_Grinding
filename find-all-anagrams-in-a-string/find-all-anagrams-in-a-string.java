4d516bef4c0e69fceb3c3522a718bbe907735d23
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-09-26 21:57:20 +0530

    Time: 20 ms (45.47%), Space: 40.1 MB (57.19%) - LeetHub

--- find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,100 +1,42 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
         
-//         HashMap<Character,Integer> h= new HashMap<Character,Integer>();
-//         for(int i=0;i<p.length();i++)
-//         {
-//             h.put(p.charAt(i),h.getOrDefault(p.charAt(i),0)+1);
-//         }
-        
-        
-//         int st=0;
-//         int end=0;
-//         List<Integer> ans = new ArrayList<Integer>();
-//         while(end<s.length())
-//         {
-//             boolean b= true;
-//             while(end<s.length())
-//             {
-//                   System.out.println(st+" "+end);
-//                int m = h.getOrDefault(s.charAt(end),0);
-             
-//                 if(m<=0)
-//                 {
-//                     b=false;
-//                     break;
-//                 }
-                
-//                 h.put(s.charAt(end),h.getOrDefault(s.charAt(end),0)-1);
-//                  end++;
-              
-//            if(end-st == p.length())
-//                  ans.add(st);
-//             }
-               
-//             if(!b)
-//             {
-//                 while(st<end)
-//                 {
-//                  h.put(s.charAt(st),h.getOrDefault(s.charAt(st),0)+1);  
-//                     st++;
-//                 }
-                
-//                 st=end;
-//             }
-//             else
-//             {
-//                st++;
-//                 end++;
-//             }
-//         }
-        
-//         return ans;
-        
-        
-        
-        // HASHMAP IS STILL NEED
-        
-            HashMap<Character,Integer> h= new HashMap<Character,Integer>();
-                int count=0;
-
-        for(char c :p.toCharArray())
-        {
+        HashMap<Character,Integer> h = new HashMap();
+        for(char c:p.toCharArray())
             h.put(c,h.getOrDefault(c,0)+1);
-            count++;
-        }
+    
+        ArrayList<Integer> list=  new ArrayList();
+        
         int st=0;
         int end=0;
-        List<Integer> ll = new ArrayList<Integer>();
+        int ans=0;
+        int count=0;
         while(end<s.length())
         {
-            char e = s.charAt(end);
-            char es =  s.charAt(st);
-            if(h.containsKey(e))
+            if(h.containsKey(s.charAt(end)))
             {
-            h.put(e,h.getOrDefault(e,0)-1);
-                if(h.get(e)>=0)
-                count--;
-            }
-            end++;
-            while(count==0)
-            {
-                    if(end-st==p.length())
-                    {
-                        ll.add(st);
-                    }
-                 es =  s.charAt(st); 
-                if(h.containsKey(es))
-                {
-                h.put(es,h.get(es)+1);
-                    if(h.get(es)>0)
+                h.put(s.charAt(end),h.get(s.charAt(end))-1);
                     count++;
-                }
-                    st++;
             }
             
+            
+                while(st<=end && (!h.containsKey(s.charAt(end)) || h.get(s.charAt(end))<0))
+                {
+//System.out.println(st+" "+end+" "+h+"**"+" "+ );
+                    if(h.containsKey(s.charAt(st)))
+                    {h.put(s.charAt(st),h.get(s.charAt(st))+1);
+                    count--;}
+                    st++;
+                }
+                
+       //   System.out.println(st+" " +end+" "+h+" "+count);
+                 if(end-st+1==p.length())
+                list.add(st);
+          
+          // System.out.println(h+" "+count+" "+st+" "+end);
+            end++;    
         }
         
-        return ll;
+        return list;
     }
 }
