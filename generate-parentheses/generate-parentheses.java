ae965fb9095926f1a58736e5e0aa24041e4490b4
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-09-27 12:33:46 +0530

    Time: 1 ms (78.47%), Space: 39.3 MB (59.07%) - LeetHub

--- generate-parentheses/generate-parentheses.java
+++ generate-parentheses/generate-parentheses.java
@@ -1,64 +1,81 @@
 class Solution {
     public List<String> generateParenthesis(int n) {
-         
-        StringBuilder sb = new StringBuilder("");
         
-        for(int i=0;i<n;i++)
-            sb.append('(');
-        
-        for(int i=0;i<n;i++)
-            sb.append(')');
-        
-        int open=0;
-        int closed =0;
-        
-        HashSet<String> set = new HashSet();
-        
-        List<String> list = new ArrayList<String>();
-        
-        fn(list,
-"()",set,open,closed,new StringBuilder(""),new HashSet<Integer>(),n);
-        
+        ArrayList<String> list=  new ArrayList();
+        fn(list,n,0,0,"");
+   //     System.out.println(list);
         return list;
     }
     
-    public void fn(List<String> res,String str,HashSet<String> set,int op,int cl,StringBuilder ans,HashSet<Integer> indices,int n)
+    public void fn(ArrayList<String> list,int n,int open,int closed,String sb)
     {
-     //   System.out.println(res+" "+ans+" "+indices);
-        
-        if(ans.length()==str.length()*n && !set.contains(ans.toString()))
+        if(open+closed==n*2)
         {
-            set.add(ans.toString());
-            res.add(ans.toString());
-            
+            list.add(new String(sb));
             return;
         }
         
+        if(open<n)
+            fn(list,n,open+1,closed,sb+"(");
         
-        for(int i=0;i<str.length();i++)
-        {
-         //   if(indices.contains(i))
-           //     continue;
-            if((str.charAt(i)==')' && op==cl) || (str.charAt(i)=='(' && op==n))
-                continue;
-            
-            ans.append(str.charAt(i));
-         //   indices.add(i);
-            int ncol=cl;
-            int nop=op;
-            
-            if(str.charAt(i)==')')
-                 ncol++;
-            else
-                nop++;
-                
-      //      System.out.println(ncol+" "+nop+" "+indices+" "+ans+"***");
-            fn(res,str,set,nop,ncol,ans,indices,n);
-            
-            ans.delete(ans.length()-1,ans.length());
-       //     indices.remove(i);
-        }
+        if(open>closed )
+            fn(list,n,open,closed+1,sb+")");
     }
+        
+         
+//         StringBuilder sb = new StringBuilder("");
+        
+//         for(int i=0;i<n;i++)
+//             sb.append('(');
+        
+//         for(int i=0;i<n;i++)
+//             sb.append(')');
+        
+//         int open=0;
+//         int closed =0;
+        
+//         HashSet<String> set = new HashSet();
+        
+//         List<String> list = new ArrayList<String>();
+        
+//         fn(list,
+// "()",set,open,closed,new StringBuilder(""),new HashSet<Integer>(),n);
+        
+//         return list;
+//     }
+    
+//     public void fn(List<String> res,String str,HashSet<String> set,int op,int cl,StringBuilder ans,HashSet<Integer> indices,int n)
+//     {
+//         if(ans.length()==str.length()*n )
+//         {
+            
+//             res.add(ans.toString());
+            
+//             return;
+//         }
+           
+//         for(int i=0;i<str.length();i++)
+//         {
+         
+//             if((str.charAt(i)==')' && op==cl) || (str.charAt(i)=='(' && op==n))
+//                 continue;
+            
+//             ans.append(str.charAt(i));
+
+//             int ncol=cl;
+//             int nop=op;
+            
+//             if(str.charAt(i)==')')
+//                  ncol++;
+//             else
+//                 nop++;
+                
+//             fn(res,str,set,nop,ncol,ans,indices,n);
+            
+//             ans.delete(ans.length()-1,ans.length());
+
+//         }
+   //   }  
 }
 
 
@@ -79,6 +96,7 @@ class Solution {
 
 
 
+(()(
 
 
 
@@ -86,27 +104,7 @@ class Solution {
 
 
 
-
-
-2 -  ()() (()) 0,0
-
-(())   
-
-  3,3
-  
-  oooccc
-  
- 
-  
-
-
-
-
-
-
-
-
-
+open==count 
 
 
 */
