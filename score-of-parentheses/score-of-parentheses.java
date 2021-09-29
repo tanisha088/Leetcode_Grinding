f4f72aa4bf4ca02c1322b9e68996e2e69d0591ed
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-09-29 23:06:48 +0530

    Time: 0 ms (100.00%), Space: 37 MB (78.41%) - LeetHub

--- score-of-parentheses/score-of-parentheses.java
+++ score-of-parentheses/score-of-parentheses.java
@@ -1,49 +1,134 @@
 class Solution {
+    
+  
+    
     public int scoreOfParentheses(String s) {
      
-        Stack<String> st =new Stack<String>();
+//         Stack<String> st =new Stack<String>();
+        
+//         for(int i=0;i<s.length();i++)
+//         {
+//             char c = s.charAt(i);
+            
+//             if(c=='(')
+//                 st.push(c+"");
+//             else
+//             {
+//                 if(st.peek().equals("("))
+//                 {
+//                     st.pop();
+                  
+//                     if(!st.isEmpty() && !st.peek().equals("("))
+//                     {
+//                       st.push((Integer.parseInt(st.pop())+1) + "");
+//                     }
+//                     else
+//                         st.push(1+"");
+//                 }
+//                 else
+//                 {
+//                     int m = Integer.parseInt(st.pop());
+//                     st.pop();
+                   
+//                     if(!st.isEmpty() && !st.peek().equals("("))
+//                     {
+//                       st.push((Integer.parseInt(st.pop())+2*m) + "");
+//                     }
+//                     else
+//                          st.push(2*m+"");
+//                 }
+//             }
+//         }
+        
+//         return Integer.parseInt(st.peek());
+        
+        
+//           int res[] = new int[30], i = 0;
+//         for (char c : s.toCharArray())
+//         {
+//             if (c == '(') res[++i] = 0;
+//             else res[i - 1] += Math.max(res[i--] * 2, 1);
+                    
+//             for(int j=0;j<res.length;j++)
+//                 System.out.print(res[j]+" ");
+//             System.out.println();
+//         }
+//         return res[0];
+        
+        
+        Stack<int[]>  st = new Stack<int[]>();
+        
+        st.push(new int[]{-1,0});
         
         for(int i=0;i<s.length();i++)
         {
-            char c = s.charAt(i);
-            
-            if(c=='(')
-                st.push(c+"");
-            else
+            if(s.charAt(i)=='(' || st.peek()[0]==-1 || s.charAt(st.peek()[0])==')')
+                st.push(new int[]{i,0});
+            else 
             {
-                if(st.peek().equals("("))
-                {
-                    st.pop();
-                  
-                    if(!st.isEmpty() && !st.peek().equals("("))
-                    {
-                      st.push((Integer.parseInt(st.pop())+1) + "");
-                    }
-                    else
-                        st.push(1+"");
-                }
-                else
-                {
-                    int m = Integer.parseInt(st.pop());
-                    st.pop();
-                   
-                    if(!st.isEmpty() && !st.peek().equals("("))
-                    {
-                      st.push((Integer.parseInt(st.pop())+2*m) + "");
-                    }
-                    else
-                         st.push(2*m+"");
-                }
+                int[] m = st.pop();
+                int ans=(m[1]==0?1:2*m[1]);
+                
+                int[] m1 =  st.peek();
+                m1[1] += ans; 
             }
         }
         
-        return Integer.parseInt(st.peek());
+        
+        return st.pop()[1];
         
     }
 }
 /*
+
 (()())(())
-score -> 4+2 = 6     4  
+score -> 4+2 = 6     4    
 */
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*
+
+
+
+
+
+(( () (()) ))   
+
+
+
+
+
+
+
+
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
