b0f533a46e61fa61323dd0f8d6ae19079edda80a
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-10-04 16:02:52 +0530

    Time: 17 ms (10.09%), Space: 68.5 MB (5.00%) - LeetHub

--- island-perimeter/island-perimeter.java
+++ island-perimeter/island-perimeter.java
@@ -1,29 +1,32 @@
 class Solution {
     public int islandPerimeter(int[][] grid) {
+     int ans=0;
         
-        int res=0;
         for(int i=0;i<grid.length;i++)
         {
             for(int j=0;j<grid[0].length;j++)
             {
-                if(grid[i][j]==0)
-                    continue;
-               int[] x= {-1,0,0,1};
-               int[] y= {0,1,-1,0};
-               int co=4;
-                for(int k=0;k<4;k++)
+                if(grid[i][j]==1)
                 {
-                    int a = x[k]+i;
-                    int b = y[k]+j;
+                    int counter=4;
+                    int[] x=  {-1,0,1,0};
+                    int[] y= {0,1,0,-1};
                     
-                    if(a>=0 && a<grid.length && b>=0 && b<grid[0].length && grid[a][b]==1)
-                        co--;
+                    for(int k=0;k<4;k++)
+                    {
+                        int nx = x[k]+i;
+                        int ny = y[k]+j;
+                        
+                        if(nx>=0 && ny>=0 && nx<grid.length && ny<grid[0].length && grid[nx][ny]==1)
+                             counter--;
+                    }
+                    
+                    ans+=counter;
                 }
-                
-                res+=co;
             }
         }
         
-        return res;
+        
+        return ans;
     }
 }
