760a72b5e75da243f4682f4ecb7c1390abfe3e7d
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-10-08 23:22:40 +0530

    Time: 0 ms (100.00%), Space: 38.1 MB (74.61%) - LeetHub

--- unique-paths-ii/unique-paths-ii.java
+++ unique-paths-ii/unique-paths-ii.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+    public int uniquePathsWithObstacles(int[][] m) {
      
         
         
-        if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]==1)
-            return 0;
+//         if(obstacleGrid[0][0]==1 || obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1]==1)
+//             return 0;
 //         for(int i=0;i<obstacleGrid.length;i++)
 //         {
 //             for(int j=0;j<obstacleGrid[0].length;j++)
@@ -57,30 +57,70 @@ class Solution {
 //         return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1];
         
         
-        for(int i=0;i<obstacleGrid.length;i++)
+//         for(int i=0;i<obstacleGrid.length;i++)
+//         {
+//             for(int j=0;j<obstacleGrid[0].length;j++)
+//             {
+//                 if(i==0 && j==0)
+//                     obstacleGrid[i][j]=1;
+//                else if(i==0 || j==0)
+//                 {
+//                     if(obstacleGrid[i][j]!=1)
+//                         obstacleGrid[i][j]=((i==0)?obstacleGrid[i][j-1]:obstacleGrid[i-1][j]);
+//                     else
+//                         obstacleGrid[i][j]=0;
+//                 }
+//                 else
+//                 {
+//                     if(obstacleGrid[i][j]==1)
+//                         obstacleGrid[i][j]=0;
+//                     else
+//                     obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
+//                 }
+//             }
+//         }
+        
+//         return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        
+        if(m[m.length-1][m[0].length-1]==1 || m[0][0]==1)
+            return 0;
+        
+        for(int i=0;i<m.length;i++)
         {
-            for(int j=0;j<obstacleGrid[0].length;j++)
+            for(int j=0;j<m[0].length;j++)
             {
-                if(i==0 && j==0)
-                    obstacleGrid[i][j]=1;
-               else if(i==0 || j==0)
-                {
-                    if(obstacleGrid[i][j]!=1)
-                        obstacleGrid[i][j]=((i==0)?obstacleGrid[i][j-1]:obstacleGrid[i-1][j]);
-                    else
-                        obstacleGrid[i][j]=0;
-                }
+                if(m[i][j]==0)
+                    m[i][j]=1;
                 else
-                {
-                    if(obstacleGrid[i][j]==1)
-                        obstacleGrid[i][j]=0;
-                    else
-                    obstacleGrid[i][j]=obstacleGrid[i-1][j]+obstacleGrid[i][j-1];
-                }
+                    m[i][j]=0;
             }
         }
         
-        return obstacleGrid[obstacleGrid.length-1][obstacleGrid[0].length-1];
+        for(int i=1;i<m.length;i++)
+        {
+            if(m[i][0]==0)
+                continue;
+            m[i][0]=m[i-1][0];
+        }
+        
+        for(int i=1;i<m[0].length;i++)
+        {
+             if(m[0][i]==0)
+                continue;
+            m[0][i] = m[0][i-1];
+        }
+        
+        for(int i=1;i<m.length;i++)
+        {
+            for(int j=1;j<m[0].length;j++)
+            {
+                if(m[i][j]==0)
+                    continue;
+                m[i][j] = m[i-1][j] + m[i][j-1];
+            }
+        }
+        
+        return m[m.length-1][m[0].length-1];
         
     }
 }
