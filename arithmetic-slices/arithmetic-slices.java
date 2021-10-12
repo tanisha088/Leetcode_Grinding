05896a42627af37a5f5c437de98a9836151d927b
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-10-12 12:53:27 +0530

    Time: 0 ms (100.00%), Space: 38.9 MB (17.49%) - LeetHub

--- arithmetic-slices/arithmetic-slices.java
+++ arithmetic-slices/arithmetic-slices.java
@@ -1,48 +1,26 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-        
         if(nums.length<3)
             return 0;
-        
-        int st=0;
-        int end=2;
-        int ans=0;
-        int c=1;
-         int prev= nums[1]-nums[0];
-        
-        while(end<nums.length)
+        int psize=2;
+        int tot=0;
+        int psum=nums[1]-nums[0];
+        for(int i=2;i<nums.length;i++)
         {
-            int diff =  nums[end]-nums[end-1] ;
-
-               if( prev!=diff )
-                {
-                st=end-1;
-                prev =diff ;
-                c=1;
-                }
-                else if (end-st+1 >=3)
-                {
-                ans =ans+c;
-                c++;
-                }
-                end++;
+            if(nums[i]-nums[i-1]!=psum)
+            {
+             psum  = nums[i]-nums[i-1];   
+                psize=2;
+            }
+            else
+            {
+                psize++;
+                tot = tot + Math.max(0,psize-2);
+            }
         }
         
-        return ans;
+        return tot;
     }
-    
-    /*
-      2 4 5 6 8 9 10
-      
-      prev=2
-      c=1
-      st=3
-      end=4
-      ans=1
-    */
-    
-    
-    
-    
 }
-
+                                     
+                                //     a b c d
