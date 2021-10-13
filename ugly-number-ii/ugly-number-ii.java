e0104a2b38e9ef5def48b4efd85c7554a8838664
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-10-13 06:32:44 +0530

    Time: 2 ms (98.87%), Space: 38 MB (81.36%) - LeetHub

--- ugly-number-ii/ugly-number-ii.java
+++ ugly-number-ii/ugly-number-ii.java
@@ -5,10 +5,16 @@ class Solution {
         arr[0]=1;   
         
         int[] pointer =  new int[3];
+        int[] nums =  new int[3];
+        
+        nums[0]=2;
+        nums[1]=3;
+        nums[2]=5;
+        
         int i=1;
         while(i<n)
         {
-            int min=Math.min(arr[pointer[0]]*2,Math.min(arr[pointer[1]]*3,arr[pointer[2]]*5));
+            int min=Math.min(arr[pointer[0]]*nums[0],Math.min(arr[pointer[1]]*nums[1],arr[pointer[2]]*nums[2]));
             
             if(arr[pointer[0]]*2==min)
                 pointer[0]++;
