f519ee467419fd15377d13a7b06c7b66edc51bd0
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-11-08 14:10:06 +0530

    Time: 25 ms (17.42%), Space: 39.2 MB (64.69%) - LeetHub

--- multiply-strings/multiply-strings.java
+++ multiply-strings/multiply-strings.java
@@ -1,100 +1,54 @@
 class Solution {
     public String multiply(String num1, String num2) {
-      
-        /*
-         1 2 3
-        x4 5 6
-             
-          7 3 8
-        6 1 5 x
-      4 9 2 x x       
-          */   
-             String str = "";
-             
-             for(int i=num2.length()-1;i>=0;i--)
-             {
-                 int carr=0;
-                 String s1 = "";
-                 for(int j=num1.length()-1;j>=0;j--)
-                 {
-                    int prod = (num1.charAt(j)-'0')*(num2.charAt(i)-'0');
-                     int nprod = (prod+carr)%10;
-                     s1 = (char)(nprod+'0')+s1;
-                     carr = (prod+carr)/10;
-                 }
-                 if(carr!=0)
-                      s1 = (char)(carr+'0')+s1;
-                 
-                 if(str.length()==0)
-                     str=s1;
-                 else
-                 {
-                     String nstr = "";
-                       int st1 = str.length()-1;
-                        int st2 = s1.length()-1;
-                     int k1  = i;
-                     while(k1!=num2.length()-1)
-                     {
-                         nstr = (str.charAt(st1)-'0')+nstr;
-                           st1--;
-                         k1++;
-                     }
-                     
-                    // nstr = str.charAt(str.length()-1)+"";
-                     carr=0;
-                //    System.out.println(nstr);
-                     
-                     while(st1>=0 && st2>=0)
-                     {
-                         int sum  =  str.charAt(st1)-'0' + s1.charAt(st2)-'0' + carr ;
-                         carr = sum/10;
-                         nstr = (char)(sum%10+'0') + nstr;
-                         st1--;
-                         st2--;
-                     }
-                     
-                     while(st2>=0)
-                     {
-                         int sum = s1.charAt(st2)-'0' + carr;
-                         carr= sum/10;
-                         nstr = (char)(sum%10+'0')+nstr;
-                         st2--;
-                     }
-                     if(carr!=0)
-                     {
-                         nstr = (char)(carr+'0')+nstr;
-                     }
-                     str = nstr;
-                 }
-                 
-              //   System.out.println(str+" "+s1);
-             }
         
-        int i1 =0;
+        int[] arr = new int[num1.length()+num2.length()+1];
+    
+        int ind1=0;
+        int ind2=0;
         
-        String cr = "";
-        
-        while(i1<str.length() && str.charAt(i1)=='0')
-              i1++;
-        
-        while(i1<str.length())
+        for(int i=num1.length()-1;i>=0;i--)
         {
-            cr = cr + str.charAt(i1);
-            i1++;
+            ind2=0;
+            for(int j=num2.length()-1;j>=0;j--)
+            {
+            arr[ind1+ind2] += (num1.charAt(i)-'0')*(num2.charAt(j)-'0');
+                ind2++;
+            }
+            ind1++;
         }
         
-        if(cr.length()==0)
-            cr="0";
+        int car=0;
+        for(int i=0;i<arr.length;i++)
+        {
+            System.out.println(arr[i]+"*");
+            int sum =  arr[i]+car;
+             car = sum/10;
+            sum = sum%10;
+           
+            arr[i] = sum;
+            
+       //     System.out.println(arr[i]);
+        }
         
-        return cr;
-             
+        StringBuilder sb=  new StringBuilder("");
+        
+        for(int i=0;i<arr.length;i++)
+        {
+            sb.append(arr[i]);
+        }
+        
+        sb.reverse();
+        
+        int i=0;
+        while(i<sb.length() && sb.charAt(i)=='0')
+             i++;
+        
+        if(i==sb.length())
+            return "0";
+        
+        
+        return sb.substring(i,sb.length()).toString();
+        
     }
-} 
+}
 
-/*
-105
- 92
-
-    105
-    92
-    */
