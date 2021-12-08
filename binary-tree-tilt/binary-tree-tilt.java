8774dbdbb0f57682f3ca9ab514b07311f3d85a0c
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2021-12-08 12:27:30 +0530

    Time: 1 ms (38.44%), Space: 42.4 MB (23.12%) - LeetHub

--- binary-tree-tilt/binary-tree-tilt.java
+++ binary-tree-tilt/binary-tree-tilt.java
@@ -18,23 +18,22 @@ class Solution {
     int sum=0;
     
     public int findTilt(TreeNode root) {
-        sum=0;
-    int[] ans =  fn(root);
+    
+        fn(root);
         
-        return ans[1];
-      //  return sum;
+        return sum;
     }
     
-    public int[] fn(TreeNode root)
+    public int fn(TreeNode root)
     {
-        if(root==null)
-            return new int[]{0,0};
+         if(root==null)
+            return 0;
         
-        int[] j1 = fn(root.left);
-        int[] j2 = fn(root.right);
-       
-       return new int[]{j1[0]+j2[0]+root.val,j1[1]+j2[1]+Math.abs(j1[0]-j2[0])};
-       
-     
+        int l = fn(root.left);
+        int r=  fn(root.right);
+        
+        sum = sum + Math.abs(l-r);
+        
+        return l+r+root.val;
     }
 }
