afe281ebf172009b70ae168f3935321584227f45
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-01-01 11:03:11 +0530

    Time: 61 ms (96.97%), Space: 38.2 MB (98.84%) - LeetHub

--- burst-balloons/burst-balloons.java
+++ burst-balloons/burst-balloons.java
@@ -13,9 +13,28 @@ class Solution {
      //   for(int i=0;i<arr.length;i++)
        //     System.out.println(arr[i]);
         int[][] dp= new int[arr.length][arr.length];
-        return comb(arr,0,arr.length-1,dp);
+        
+        for(int gap=2;gap<arr.length;gap++)
+        {
+            for(int left=0;left<arr.length-gap;left++)
+            {
+               int right = left + gap;
+                int ans=0;
+                for(int i=left+1;i<right;i++)
+                {
+                    ans=Math.max(ans,dp[left][i]+dp[i][right]+arr[left]*arr[right]*arr[i]);
+                }
+                dp[left][right]=ans;
+            }
+        }
+        
+        return dp[0][arr.length-1];
+        
+        //CALL FOR THE CASES OF MEMORIZATION AND NON-MEMORIZATION
+      //  return comb(arr,0,arr.length-1,dp);
     }
-    
+    // WITH MEMORIZATION 
+    /*
     public int comb(int[] nums,int left,int right,int[][] dp)
     {
         if(dp[left][right]>0)
@@ -32,4 +51,23 @@ class Solution {
         dp[left][right]=ans;
         return ans;
     }
+    */
+    
+    /*
+    // WITHOUT MEMORIZATION
+         public int comb(int[] nums,int left,int right,int[][] dp)
+    {
+       
+        int ans=0;
+        for(int i=left+1;i<right;i++)
+        {
+            int leftsum = comb(nums,left,i,dp);
+            int rightsum = comb(nums,i,right,dp);
+            ans=Math.max(ans,leftsum+rightsum+nums[left]*nums[right]*nums[i]);
+            
+        }
+     
+        return ans;
+    }    
+    */
 }
