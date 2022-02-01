ddd45ccb6b1e02d5ec0517e9c02421c18b1c1c33
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-02-01 07:33:27 +0530

    Time: 0 ms (100.00%), Space: 41.2 MB (5.21%) - LeetHub

---
+++ 509-fibonacci-number/509-fibonacci-number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+        
+        if(n==0 || n==1)
+            return n;
+        
+        
+        
+        int[] fib = new int[n+1];
+        fib[0]=0;
+        fib[1]=1;
+        for(int i=2;i<=n;i++)
+        {
+            fib[i]=fib[i-1]+fib[i-2];
+        }
+        
+        return fib[n];
+    }
+}
