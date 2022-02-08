83613e9ee70668c7c147c94c34d0eb9510bf5fed
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-02-08 09:25:12 +0530

    Time: 2 ms (59.15%), Space: 41.1 MB (13.44%) - LeetHub

---
+++ 258-add-digits/258-add-digits.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int addDigits(int num) {
+        
+          if(num<=9)
+              return num;
+        
+        int sum=0;
+        while(num>0)
+        {
+            sum =  sum + num%10;
+            num = num/10;
+        }
+        
+        return addDigits(sum);
+    }
+}
+
+
+
