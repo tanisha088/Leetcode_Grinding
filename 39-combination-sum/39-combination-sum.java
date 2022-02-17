9f8fa207253904e2220c219d6bb2f301cc8b37d7
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-02-17 09:52:26 +0530

    Time: 7 ms (43.89%), Space: 44.7 MB (27.19%) - LeetHub

--- 39-combination-sum/39-combination-sum.java
+++ 39-combination-sum/39-combination-sum.java
@@ -2,11 +2,8 @@ class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
         
         List<List<Integer>> list =  new ArrayList();
-        
-        Arrays.sort(candidates);
+      //  Arrays.sort(candidates);
         fn(candidates,target,0,new ArrayList<Integer>(),list,0);
-        
-      
         return list;
     }
   
@@ -17,16 +14,12 @@ class Solution {
             ans.add(new ArrayList<Integer>(l1));
             return;
         }
-        
         if(sum>target)
             return;
-        
         for(int i=j;i<candidates.length;i++)
         {
             l1.add(candidates[i]);
-            
             fn(candidates,target,sum+candidates[i],l1,ans,i);
-            
             l1.remove(l1.size()-1);
         }
     }
