6e167566f9ea04cafe1dea9870e8315201db6ade
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-02-28 10:13:01 +0530

    Time: 1 ms (90.98%), Space: 42 MB (38.03%) - LeetHub

--- 228-summary-ranges/228-summary-ranges.java
+++ 228-summary-ranges/228-summary-ranges.java
@@ -1,11 +1,8 @@
 class Solution {
     public List<String> summaryRanges(int[] nums) {
-        
         Arrays.sort(nums);
-        
         int i= 0 ;
         List<String> ll =  new ArrayList();
-        
         while(i<nums.length)
         {
             StringBuilder bs = new StringBuilder("");
@@ -14,17 +11,14 @@ class Solution {
             int j =  i;
             while(i<nums.length && nums[i]==nums[i-1]+1)
                 i++;
-            
           if(i!=j)
             {
                 bs.append("->");
                 bs.append(nums[i-1]);
             }
-            
             ll.add(bs.toString());
-   
         }
-        
         return ll;
     }
 }
+
