0c57a86d16e05984537be9efcca33b4824d4c7fc
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-03-03 10:20:00 +0530

    Time: 0 ms (100.00%), Space: 40.2 MB (46.73%) - LeetHub

--- 413-arithmetic-slices/413-arithmetic-slices.java
+++ 413-arithmetic-slices/413-arithmetic-slices.java
@@ -1,32 +1,23 @@
 class Solution {
     public int numberOfArithmeticSlices(int[] nums) {
-  
         if(nums.length<3)
-            return 0;
-        
-        int  i = 0;
+            return 0; 
+        int  i = 1;
         int count=0;
         int iter =0;
-        int diff = nums[1]- nums[0];
-        i=2;
         while(i<nums.length)
         {
+              int diff = nums[i]- nums[i-1];
+              i++;
+              iter=0;
             while(i<nums.length && nums[i]-nums[i-1]==diff)
             {
                 i++;
                 iter++;
                 count = count + iter;
             }
-            
-            if(i==nums.length)
-                break;
-            diff = nums[i] - nums[i-1];
-            i++;
-            iter=0;
         }
-        
         return count;
-        
     }
 }
                                      
