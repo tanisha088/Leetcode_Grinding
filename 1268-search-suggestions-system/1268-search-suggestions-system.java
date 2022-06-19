3d7572c98aeae5b39a517da18ff10287bfacb6e7
Tanisha Rastogi <31798751+tanisha088@users.noreply.github.com>
2022-06-19 12:29:47 +0530

    Time: 50 ms (57.86%), Space: 45.6 MB (94.21%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,47 @@
+class Solution {
+    
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+        
+        HashMap<Integer,ArrayList<String>> map=  new HashMap();
+        int maxCount = -1 ;
+        for(int i=0;i<products.length;i++)
+        {
+            String str =  products[i];
+            int count = 0;
+            for(char c:str.toCharArray())
+            {
+                if(count>=searchWord.length() || searchWord.charAt(count)!=c)
+                 break;
+                
+                if(!map.containsKey(count))
+                    map.put(count,new ArrayList());
+                
+                ArrayList<String> ll = map.get(count);
+                ll.add(str);
+                map.put(count,ll);
+                maxCount = Math.max(maxCount,count);
+                count++;
+            }
+            
+        }
+        
+        List<List<String>> ans = new ArrayList();
+        
+        for(int i=0;i<searchWord.length();i++)
+        {
+             ArrayList<String> templist;
+            if(i>maxCount)
+                templist = new ArrayList();
+            else
+                templist = map.get(i);
+            Collections.sort(templist);
+            List<String> parts =  new ArrayList();
+            for(int j=0;j<Math.min(3,templist.size());j++)
+                parts.add(templist.get(j));
+            
+            ans.add(parts);
+        }
+        
+        return ans;
+    }
+}
